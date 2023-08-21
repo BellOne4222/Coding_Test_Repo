bbfd9d4c359ef678a71879f7120ec76447d07852
JongWon Kim <127610150+BellOne4222@users.noreply.github.com>
2023-08-21 19:35:02 +0900

    8/21

--- practice.py
+++ practice.py
@@ -1,46 +1,15 @@
-from collections import deque
+N = int(input())
 
+stairs = [0]
+for i in range(1, N+1):
+    stairs.append(int(input()))
+    
+dp_table = [0] * (N+1)
 
-def solution(rows, columns, queries):
-    arr = [[0] * columns for _ in range(rows)]
-    cnt = 0
-    answer = []
-    for i in range(rows):
-        for j in range(columns):
-            cnt += 1
-            arr[i][j] = cnt
+dp_table[1] = stairs[1]
+dp_table[2] = stairs[1] + stairs[2]
 
-    for qur in queries:
-        q = deque()
-        x1, y1, x2, y2 = qur
+for j in range(3,N+1):
+    dp_table[j] = max(stairs[j] + stairs[j-1] + dp_table[j-3], stairs[j] + dp_table[j-2])
 
-        for i in range(y1 - 1, y2):
-            q.append(arr[x1 - 1][i])
-
-        for i in range(x1, x2):
-            q.append(arr[i][y2 - 1])
-
-        for i in range(y2 - 2, y1 - 2, -1):
-            q.append(arr[x2 - 1][i])
-
-        for i in range(x2 - 2, x1 - 1, -1):
-            q.append(arr[i][y1 - 1])
-
-        q.rotate(1)
-        answer.append(min(q))
-
-        for i in range(y1 - 1, y2):
-            arr[x1 - 1][i] = q.popleft()
-
-        for i in range(x1, x2):
-            arr[i][y2 - 1] = q.popleft()
-
-        for i in range(y2 - 2, y1 - 2, -1):
-            arr[x2 - 1][i] = q.popleft()
-
-        for i in range(x2 - 2, x1 - 1, -1):
-            arr[i][y1 - 1] = q.popleft()
-
-    return answer
-
-print(solution(6,6,[[2,2,5,4],[3,3,6,6],[5,1,6,3]]))
+print(dp_table[N])
