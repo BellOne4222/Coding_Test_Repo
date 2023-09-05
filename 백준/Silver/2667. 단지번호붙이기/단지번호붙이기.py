4d8a75006eae0444310478816dcae1fbd41245f1
JongWon Kim <127610150+BellOne4222@users.noreply.github.com>
2023-09-05 19:12:10 +0900

    9/5

--- 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.py
+++ 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.py
@@ -2,46 +2,49 @@
 
 n = int(input())
 graph = []
-num = []
+houses = [] # 집 개수를 받을 배열
+
 
 for i in range(n):
-    graph.append(list(map(int, input())))
+    graph.append(list(map(int,input())))
 
-dx = [0, 0, 1, -1]
-dy = [1, -1, 0, 0]
-
-
-def DFS(x, y):
+dx = [-1,1,0,0]
+dy = [0,0,-1,1]    
+    
+def dfs(x,y):
+    # 범위 벗어나면 False
     if x < 0 or x >= n or y < 0 or y >= n:
         return False
-
+    
+    # 그래프의 칸이 1이면 집이 있는 곳이므로 집 개수 +=1
     if graph[x][y] == 1:
-        global count
-        count += 1
+        global house_cnt
+        house_cnt += 1
+        # 세었으면 방문 처리
         graph[x][y] = 0
+
+        # 상하좌우로 dfs 실행해서 탐색
         for i in range(4):
             nx = x + dx[i]
             ny = y + dy[i]
-            DFS(nx, ny)
+            dfs(nx,ny)
         return True
     return False
+    
 
+house_cnt = 0 # 집 개수
+village_cnt = 0 # 마을 개수
 
-count = 0
-result = 0
+# True이면 집 개수 배열에 추가하고 마을 개수 += 1 하고 집 개수 초기화
+for j in range(n):
+    for k in range(n):
+        if dfs(j,k) == True:
+            houses.append(house_cnt)
+            village_cnt += 1
+            house_cnt = 0
 
-for i in range(n):
-    for j in range(n):
-        if DFS(i, j) == True:
-            num.append(count)
-            result += 1
-            count = 0
-
-num.sort()
-print(result)
-for i in range(len(num)):
-    print(num[i])
-
-
-            
-
+# 오름차순 정렬
+houses.sort()
+print(village_cnt)
+for l in range(len(houses)):
+    print(houses[l])
