e4f0106fd46031a3cbfa0ec3bb4ce97b32af1470
JongWon Kim <127610150+BellOne4222@users.noreply.github.com>
2023-09-06 16:14:03 +0900

    9/6

--- practice copy 3.py
+++ practice copy 3.py
@@ -1,46 +1,18 @@
-from collections import Counter
-
-def solution(n, wires):
-    n_dict = {i:0 for i in range(1,n+1)}
-    for j in range(len(wires)):
-        n_dict[wires[j][0]] += 1
+t = int(input())
+for _ in range(t):
+    n = int(input())
+    grade = []
+    for _ in range(n):
+        grade.append(list(map(int, input().split())))
     
-    n_lst = list(n_dict)
-    # 	{1: 1, 2: 1, 3: 1, 4: 3, 5: 0, 6: 0, 7: 2, 8: 0, 9: 0}
+    grade.sort(key=lambda x: x[0])  
     
-    for k in n_lst:
-        if n_dict[k] == 0:
-            del n_dict[k]
-    # 	{1: 1, 2: 1, 3: 1, 4: 3, 7: 2}
+    cnt = 1
+    min_y = grade[0][1]
     
-    m_lst = list(n_dict) 
-    # 	[1, 2, 3, 4, 7]
+    for i in range(1, n):
+        if grade[i][1] > min_y:
+            min_y = grade[i][1]
+            cnt += 1
     
-    tower_lst = list(n_dict.values())
-    
-    common = Counter(tower_lst)
-    
-    m_common = common.most_common(1)
-    
-    most = m_common[0][0] # 1
-    
-    for l in m_lst:
-        n_dict[l] -= most
-    # [0, 0, 0, 2, 1]
-    result = sum(list(n_dict.values()))
-    
-    return result
-    
-# 테스트 1 〉	실패 (0.06ms, 10.2MB)
-# 테스트 2 〉	실패 (0.06ms, 10.1MB)
-# 테스트 3 〉	실패 (0.07ms, 10.2MB)
-# 테스트 4 〉	실패 (0.06ms, 10.2MB)
-# 테스트 5 〉	실패 (0.09ms, 10.4MB)
-# 테스트 6 〉	통과 (0.03ms, 10.1MB)
-# 테스트 7 〉	실패 (0.04ms, 10.1MB)
-# 테스트 8 〉	실패 (0.06ms, 10.2MB)
-# 테스트 9 〉	실패 (0.06ms, 10.2MB)
-# 테스트 10 〉	실패 (0.09ms, 10.2MB)
-# 테스트 11 〉	실패 (0.06ms, 10.2MB)
-# 테스트 12 〉	실패 (0.06ms, 10.1MB)
-# 테스트 13 〉	실패 (0.10ms, 10.2MB)
+    print(cnt)
--- practice.py
+++ practice.py
@@ -1,43 +1,19 @@
-n = int(input())
+from collections import deque
 
-houses = []
-graph = []
-for _ in range(n):
-    graph.append(list(map(int,input())))
-
-dx = [-1,1,0,0]
-dy = [0,0,-1,1]    
-    
-def dfs(x,y):
-    
-    if x < 0 or x >= n or y < 0 or y >= n:
-        return False
-    
-    if graph[x][y] == 1:
-        global house_cnt
-        house_cnt += 1
-        graph[x][y] = 0
-        for i in range(4):
-            nx = x + dx[i]
-            ny = y + dy[i]
-            dfs(nx,ny)
-        return True
-    return False
-    
-
-house_cnt = 0
-village_cnt = 0
-
-
-for j in range(n):
-    for k in range(n):
-        if dfs(j,k) == True:
-            houses.append(house_cnt)
-            village_cnt += 1
-            houses_cnt = 0
-
-# 오름차순 정렬
-houses.sort()
-print(village_cnt)
-for l in range(len(houses)):
-    print(houses[l])
+t = int(input())
+for _ in range(t):
+    n = int(input())
+    grade = deque()
+    for _ in range(n):
+        grade.append(list(map(int,input().split())))
+    end = grade[-1]
+    while True:
+        standard = grade.popleft()
+        for l in grade:
+            if standard[0] < l[0] and standard[1] < l[1]:
+                del grade[l]
+        grade.append(standard)
+                
+        if standard == end:
+            break
+    print(len(grade))
