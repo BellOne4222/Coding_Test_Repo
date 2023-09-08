5a5ab06267c53f1248a455b7038056bf03bb6b78
JongWon Kim <127610150+BellOne4222@users.noreply.github.com>
2023-09-08 16:10:55 +0900

    9/8

--- practice copy 2.py
+++ practice copy 2.py
@@ -1,18 +1,22 @@
-t = int(input())
-for _ in range(t):
-    n = int(input())
-    grade = []
-    for _ in range(n):
-        grade.append(list(map(int,input().split())))
-    grade.sort(key = lambda x:x[0])
-    standard_x = grade[0][0]
-    standard_y = grade[0][1]
-    cnt = 1
-    for i in range(1,len(grade)):
-        if grade[i][0] > standard_x and grade[i][1] > standard_y:
-            continue
-        else:
-            if standard_y > grade[i][1]:
-                standard_y = grade[i][1]
-                cnt += 1
-    print(cnt)
+def solution(tickets):
+    answer = []
+    
+    visited = [False]*len(tickets)
+    
+    def dfs(airport, path):
+        if len(path) == len(tickets)+1:
+            answer.append(path)
+            return
+        
+        for idx, ticket in enumerate(tickets):
+            if airport == ticket[0] and visited[idx] == False:
+                visited[idx] = True
+                dfs(ticket[1], path+[ticket[1]])
+                visited[idx] = False
+        
+        
+    dfs("ICN", ["ICN"])
+    
+    answer.sort()
+
+    return answer[0]
