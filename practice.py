d3a2665ef500c8a6b090f0f104474481a3e96d62
JongWon Kim <127610150+BellOne4222@users.noreply.github.com>
2023-09-11 17:36:41 +0900

    9/11

--- practice.py
+++ practice.py
@@ -1,32 +1,35 @@
-from collections import deque
+# 풀이 참고 : https://hi0seon.tistory.com/entry/%ED%94%84%EB%A1%9C%EA%B7%B8%EB%9E%98%EB%A8%B8%EC%8A%A4-%EC%97%AC%ED%96%89%EA%B2%BD%EB%A1%9C-%ED%8C%8C%EC%9D%B4%EC%8D%AC-DFS
 
+# dfs, defaultdict 사용
+
+
+from collections import defaultdict
+
+# 항공권 정보를 입력으로 받아 여행 경로를 찾는 함수
 def solution(tickets):
-    # 도착지 기준으로 정렬하고, 같으면(경로가 2개 이상일 경우 알파벳 순서가 앞서는 경로를 선택해야하기 때문에) 출발지 기준으로 정렬
-    tickets.sort(key = lambda x:(x[1], x[0])) 
-    
-    # 노드 번호가 아니라 공항 이름으로 노드 이름이 나와있어서 트리 구조를 만들기 위해 dictionary 사용
-    airport_dict = {}
-    for i in range(len(tickets)):
-        airport_dict[tickets[i][0]] = tickets[i][1]
-    # 	{'JFK': 'HND', 'HND': 'IAD', 'ICN': 'JFK'}
-    
-    result = deque()
-    
-    # 처음 시작 위치는 ICN 이므로 ICN 먼저 넣어준다.
-    path = ["ICN"]
-    result.append("ICN")
-    
-    while True:
-        if len(result) >= 2 and result[-2] == result[-1]:
-            result.pop()
-            break
-        start = path[-1]
-        if airport_dict[start] not in airport_dict:
-            result.appendleft(airport_dict[start])
-        else:
-            result.appendleft(airport_dict[start])
-            path.append(airport_dict[start])
-    
-    return result
+    result = []  # 여행 경로를 저장할 리스트
+    tickets.sort(key=lambda x: (x[1], x[0]))  # 항공권을 목적지를 기준으로 정렬, 목적지가 같다면 출발지를 기준으로 정렬
+    airport_dict = defaultdict(list)  # 출발 공항을 키로 가지고 목적 공항 리스트를 값으로 갖는 딕셔너리 생성
+
+    # 항공권 정보를 딕셔너리에 저장
+    for [start, end] in tickets:
+        airport_dict[start].append(end)
+
+    # 출발 공항별 목적 공항 리스트를 내림차순으로 정렬
+    for k in airport_dict.keys():
+        airport_dict[k].sort(reverse=True)
+
+    # 깊이 우선 탐색 함수 정의
+    def DFS():
+        path = ["ICN"]  # 경로를 저장할 스택, 초기값은 ICN 공항
+        while path:
+            start = path[-1]  # 현재 위치는 스택의 맨 위에 있는 공항
+            if not airport_dict[start]:  # 현재 위치에서 더 이상 갈 수 있는 공항이 없으면
+                result.append(path.pop())  # 경로에서 현재 위치를 제거하고 결과 리스트에 추가
+            else:
+                path.append(airport_dict[start].pop())  # 현재 위치에서 갈 수 있는 공항이 있으면 스택에 추가
+
+    DFS()  # DFS 함수를 호출하여 여행 경로를 찾음
+    return result[::-1]  # 결과 리스트를 역순으로 반환 (ICN부터 출발하는 경로)
 
 print(solution([["ICN", "JFK"], ["HND", "IAD"], ["JFK", "HND"]]))
