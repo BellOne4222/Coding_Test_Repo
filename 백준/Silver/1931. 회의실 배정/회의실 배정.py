c4be08e5f0befb7ad899d0cf15f68d0130d46a9b
JongWon Kim <127610150+BellOne4222@users.noreply.github.com>
2023-09-13 18:41:59 +0900

    9/13

--- 백준/Silver/1931. 회의실 배정/회의실 배정.py
+++ 백준/Silver/1931. 회의실 배정/회의실 배정.py
@@ -11,6 +11,7 @@ discuss= []
 for _ in range(n):
     discuss.append(list(map(int,input().split())))
     
+# 회의가 일찍끝나는 순 정렬, 끝나는 시간이 같다면 회의 시작이 더 빠른 순으로 정렬
 discuss.sort(key = lambda x:(x[1], x[0]))
 
 ing = discuss[0][1]
