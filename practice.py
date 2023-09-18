56975701894d641c3586d8cd0b05732f71f40218
JongWon Kim <127610150+BellOne4222@users.noreply.github.com>
2023-09-18 18:57:10 +0900

    9/18

--- practice.py
+++ practice.py
@@ -1,31 +1,41 @@
-# n = input()
-n = "quackquackquackquackquackquackquackquackquackquack"
-n = str(n)
+# n = int(input())
+n = 7
+# m = int(input())
+m = 35
 
-right = "quack"
+graph = [[0 for _ in range(n)] for _ in range(n)]
 
-idx = 0
+# 1은 그래프의 중간점이므로 그래프의 중간 위치를 1로 초기화
+x = n // 2
+y = n // 2
+graph[x][y] = 1
 
-pair = 0
+# 오른쪽, 아래쪽, 왼쪽, 위쪽 순서
+dx = [0, 1, 0, -1]
+dy = [1, 0, -1, 0]
 
-alright = 0
+# 1은 이미 초기화 시켜줬고 다음 값은 2부터 시작
+num = 1
 
-is_right = 0
+# 한 바퀴 돌때마다(달팽이) 반복 횟수가 2씩 증가하기 때문에 반복 횟수를 받을 변수
+repeat = 0
 
-duck = 0
-for i in range(len(n)):
-    if n[i] == right[idx]:
-        alright += 1
-        is_right += 1
-        idx += 1
-    if alright == 5:
-        idx = 0
-        alright = 0
-        duck += 1
-    if duck >= 2 and is_right == i+1:
-        duck -= 1
+while True:
+    for i in range(4):
+        for _ in range(repeat):  # 특정 방향으로 한칸씩 이동하며 숫자 입력
+            x += dx[i]
+            y += dy[i]
+            num += 1
+            graph[x][y] = num
+            if num == m:  # 찾을 번호의 인덱스 저장
+                ans = [x+1, y+1]
 
-if duck == 0:
-    print(-1)
-else:
-    print(duck)
+    if x == y == 0:
+        break
+    x -= 1
+    y -= 1
+    repeat += 2
+
+for j in range(n):
+    print(*graph[j])
+print(*ans)
--- 백준/Silver/12933. 오리/(1차 failed) 오리.py
+++ 백준/Silver/12933. 오리/(1차 failed) 오리.py
@@ -1,39 +1,31 @@
-# all(iterable) 함수는 인자로 받은 반복 가능한 자료형(iterable)의 모든 요소가 참(True)이면 참(True)을 반환하는 함수 입니다.
-# any(iterable) 함수는 인자로 받은 반복가능한 자료형(iterable)중 단 하나라도 참(True)이 있으면 참(True)를 반환하는 함수 입니다. 반대로 모든 요소가 거짓(False)인 경우에만 거짓(False)을 반환합니다.
-# https://velog.io/@gilyeon00/%EB%B0%B1%EC%A4%80-12933%EB%B2%88-%EC%98%A4%EB%A6%AC-python-%EC%8B%A4%EB%B2%843?ref=codenary 참고
+# n = input()
+n = "quackquackquackquackquackquackquackquackquackquack"
+n = str(n)
 
-sound = list(input())
-ans = 0
+right = "quack"
 
-# sound 문자열의 시작이 q가 아니거나 문자열 끝이 k가 아니거나 문자열의 길이가 5의 배수가 아니면 -1반환하고 프로그램 종료
-if sound[0] != "q"  or  sound[-1] != "k" or len(sound) % 5 :
+idx = 0
+
+pair = 0
+
+alright = 0
+
+is_right = 0
+
+duck = 0
+for i in range(len(n)):
+    if n[i] == right[idx]:
+        alright += 1
+        is_right += 1
+        idx += 1
+    if alright == 5:
+        idx = 0
+        alright = 0
+        duck += 1
+    if duck >= 2 and is_right == i+1:
+        duck -= 1
+
+if duck == 0:
     print(-1)
-    exit()
-
-def find_quack(start) :
-    quack = "quack"
-    j = 0
-    global ans 
-    new_ori = True      # 새로운 오리로 생성하라는 flag
-    for i in range(start, len(sound)):
-        if sound[i] == quack[j]:
-            if sound[i] == "k":
-                if new_ori:     # 새로운 오리일 경우, 오리(ans) 추가
-                    ans += 1
-                    new_ori = False     # k값으로 끝났으니 새로운 오리를 생성하지 말라는 flag          
-                j = 0           # 이어지는 "q" 탐색 - 새로운 오리가 아님 (현재 오리)
-                sound[i] = 0           
-                continue        
-            j += 1
-            sound[i] = 0
-
-
-for i in range(len(sound) - 4):     # 어차피 q 뒤에는 무조건 uack이 와야하므로 -4를 해준다
-    if sound[i] == "q" :
-        find_quack(i)
-
-
-if any(sound) or ans == 0 :
-    print(-1)
-else :
-    print(ans)
+else:
+    print(duck)
