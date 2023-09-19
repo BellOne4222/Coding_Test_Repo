80c0baa9eee6e4069b63022552e45fa09c424bd0
JongWon Kim <127610150+BellOne4222@users.noreply.github.com>
2023-09-19 18:55:06 +0900

    [Silver III] Title: 파일 정리, Time: 2048 ms, Memory: 43740 KB -BaekjoonHub

---
+++ 백준/Silver/20291. 파일 정리/파일 정리.py
@@ -0,0 +1,15 @@
+n = int(input())
+
+file = dict()
+for _ in range(n):
+    extend = (input().split('.'))[1]
+    if not extend in file:
+        file[extend] = 1
+    else:
+        file[extend] += 1
+
+sort_file = sorted(file.items())
+
+for key, value in sort_file:
+    print(key, value)
+        
