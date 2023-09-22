efcb22cec8e85be5c4baa2e60a31cc5ab217a848
JongWon Kim <127610150+BellOne4222@users.noreply.github.com>
2023-09-22 17:27:18 +0900

    9/22

--- 백준/Gold/21608. 상어 초등학교/상어 초등학교.py
+++ 백준/Gold/21608. 상어 초등학교/상어 초등학교.py
@@ -1,16 +1,16 @@
-n = int(input())
-
-classroom = [[0 for _ in range(n)] for _ in range(n)]
-
-
 # 비어있는 칸 중에서 좋아하는 학생이 인접한 칸에 가장 많은 칸으로 자리를 정한다.
 # 1을 만족하는 칸이 여러 개이면, 인접한 칸 중에서 비어있는 칸이 가장 많은 칸으로 자리를 정한다.
 # 2를 만족하는 칸도 여러 개인 경우에는 행의 번호가 가장 작은 칸으로, 그러한 칸도 여러 개이면 열의 번호가 가장 작은 칸으로 자리를 정한다.
+
+n = int(input())
+
+classroom = [[0 for _ in range(n)] for _ in range(n)]
 
 dr = [-1,1,0,0]
 dc = [0,0,-1,1]
 
 students = [list(map(int, input().split())) for _ in range(n*n)]
+
 
 
 # 자리 배치
