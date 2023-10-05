76f8abe0b27e9c7855c3563b710d1c473c98da36
JongWon Kim <127610150+BellOne4222@users.noreply.github.com>
2023-10-05 19:41:34 +0900

    10/5

--- practice copy.py
+++ practice copy.py
@@ -1,15 +1,15 @@
 n = int(input())
 towers = list(map(int, input().split()))
 stack = []
-answer = [0 for i in range(n)]
+result = [0 for i in range(n)]
  
 for i in range(n):
     while stack:
         if stack[-1][1] > towers[i]:
-            answer[i] = stack[-1][0] + 1
+            result[i] = stack[-1][0] + 1
             break
         else:
             stack.pop()
     stack.append([i, towers[i]])
  
-print(*answer)
+print(" ".join(map(str,result)))
--- practice.py
+++ practice.py
@@ -1,39 +1,19 @@
+# 현재 Queue의 가장 앞에 있는 문서의 ‘중요도’를 확인한다.
+# 나머지 문서들 중 현재 문서보다 중요도가 높은 문서가 하나라도 있다면, 이 문서를 인쇄하지 않고 Queue의 가장 뒤에 재배치 한다. 그렇지 않다면 바로 인쇄를 한다.
+# 예를 들어 Queue에 4개의 문서(A B C D)가 있고, 중요도가 2 1 4 3 라면 C를 인쇄하고, 다음으로 D를 인쇄하고 A, B를 인쇄하게 된다.
+
+# 여러분이 할 일은, 현재 Queue에 있는 문서의 수와 중요도가 주어졌을 때, 어떤 한 문서가 몇 번째로 인쇄되는지 알아내는 것이다. 예를 들어 위의 예에서 C문서는 1번째로, A문서는 3번째로 인쇄되게 된다.
+from collections import deque
+
 n = int(input())
 
-towers = []
-lst = list(map(int,input().split(" ")))
-result = [0 for _ in range(n)]
-m_height = 0
-m_height_num = 0
-
-
 for i in range(n):
-    height = lst[i]
-    if i == 0:
-        towers.append(lst[i])
-        result[i] = 0
-        
-        m_height = height
-        m_height_num = i+1
-
     
-    if i != 0 and towers[-1] < height:
-        if m_height >= height:
-            towers.append(height)
-            
-            result[i] = m_height_num
-            m_height = height
-            m_height_num = i+1
-        else:
-            m_height = height
-            m_height_num = i+1
-            towers.append(lst[i])
-            result[i] = 0
-    else:
-        if i == 0:
-            continue
-        else:
-            towers.append(height)
-            result[i] = m_height_num
+    docu, location = map(int,input().split()) # 문서 개수, 큐에서의 위치
+    order = list(map(int,input().split())) # 중요도
+    printer = {j:0 for j in range(docu)}
+    
 
-print(' '.join(map(str,result)))
+
+
+
