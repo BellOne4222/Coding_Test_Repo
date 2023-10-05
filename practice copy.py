eb8527e3095095d370e096202f754fcd5e6398cf
JongWon Kim <127610150+BellOne4222@users.noreply.github.com>
2023-10-05 17:56:23 +0900

    10/5

--- practice copy.py
+++ practice copy.py
@@ -1,82 +1,20 @@
-count=int(input())
+n = int(input())
+building = 0
 
-n=input()
+heights = []
+for i in range(n):
+    x,y = map(int,input().split())
+    heights.append(y)
+heights.append(0)
 
-l=[]
-
-for j in range(count*2+3):
-
-    l.append([])
-
-
-
-for i in range(len(n)):
-
-    if (n[i] in ['0','2','3','5','6','7','8','9']):
-
-        l[0].append(" "+count*"-"+" ")
-
-    else:
-
-        l[0].append(" "+count*" "+" ")	#뚜껑
-
-	
-
-    if (n[i] in ['0','4','8','9']):
-
-        for k in range(1,count+1):
-
-            l[k].append("|"+count*" "+"|")
-
-    elif (n[i] in ['5','6']):
-
-        for k in range(1,count+1):
-
-            l[k].append("|"+count*" "+" ")
-
-    else:
-
-        for k in range(1,count+1):
-
-            l[k].append(" "+count*" "+"|")	#위쪽 양옆
-
-	#중간
-    if (n[i] in ['0','1','7']):
-    	l[count+1].append(" "*(count+2))
-    else:
-        l[count+1].append(" "+"-"*count+" ")
-			
-
-    if(n[i] in ['0','6','8']):	
-
-        for k in range(count+2,2*count+2):
-
-            l[k].append("|"+count*" "+"|")
-
-    elif(n[i]=='2'):
-
-        for k in range(count+2,2*count+2):
-
-            l[k].append("|"+count*" "+" ")
-
-    else:
-
-        for k in range(count+2,2*count+2):
-
-            l[k].append(" "+count*" "+"|")	#아래 양옆
-
-		 
-
-    if(n[i] in ['0','2','3','5','6','8','9']):		
-
-        l[-1].append(" "+count*"-"+" ")
-
-    else:
-
-        l[-1].append(" "+count*" "+" ")	#바닥
-
-		 
-
-for k in l:
-
-	print(" ".join(k))
+cnts = []
+cnts.append(0)
+for h in heights:
+    height = h
+    while cnts[-1] > h:
+        if cnts[-1] != height:
+            building += 1
+            height = cnts[-1]
+        cnts.pop()
+    cnts.append(h)   
+print(building)
