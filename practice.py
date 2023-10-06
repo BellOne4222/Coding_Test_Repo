dd22331bbbc7f369924baa2dc403375d7b9a6922
JongWon Kim <127610150+BellOne4222@users.noreply.github.com>
2023-10-06 19:58:22 +0900

    10/6

--- practice.py
+++ practice.py
@@ -1,48 +1,34 @@
-# 현재 Queue의 가장 앞에 있는 문서의 ‘중요도’를 확인한다.
-# 나머지 문서들 중 현재 문서보다 중요도가 높은 문서가 하나라도 있다면, 이 문서를 인쇄하지 않고 Queue의 가장 뒤에 재배치 한다. 그렇지 않다면 바로 인쇄를 한다.
-# 예를 들어 Queue에 4개의 문서(A B C D)가 있고, 중요도가 2 1 4 3 라면 C를 인쇄하고, 다음으로 D를 인쇄하고 A, B를 인쇄하게 된다.
-
-# 여러분이 할 일은, 현재 Queue에 있는 문서의 수와 중요도가 주어졌을 때, 어떤 한 문서가 몇 번째로 인쇄되는지 알아내는 것이다. 예를 들어 위의 예에서 C문서는 1번째로, A문서는 3번째로 인쇄되게 된다.
 from collections import deque
 
 n = int(input())
+sentence = deque()
 
-for i in range(n):
-    
-    docu, location = map(int,input().split()) # 문서 개수, 큐에서의 위치
-    order = list(map(int,input().split())) # 중요도
-    order = deque(order)
-    my_order = 0
-    
-    while True:
-        m = max(order)
-        location -= 1
-        if order[0] == m:
-            printer = order.popleft()
-            my_order += 1
-            if location < 0:
-                print(my_order)
-                break
+
+for _ in range(n):
+    cursor = 0
+    l = list(input())
+    for i in range(len(l)):
+
+        # 왼쪽 화살표
+        if l[i] == "<":
+            cursor -= 1
+            if cursor < 0:
+                cursor = 0
+        # 오른쪽 화살표
+        elif l[i] == ">":
+            cursor += 1
+            if cursor > len(sentence):
+                cursor = len(sentence)
+        # 백스페이스
+        elif l[i] == "-":
+            cursor -= 1
+            if sentence[cursor].isalnum():
+                sentence.pop()
+
+        # 대문자, 소문자, 숫자
         else:
-            order.rotate(-1)
-            if location < 0:
-                location = docu + location
-
-
-        
-
-
-
-
-
-
-
-            
-
-
-
+            sentence.insert(cursor, l[i])
+            cursor += 1
     
-
-
-
-
+    sentence = list(sentence)
+    print(''.join(map(str,sentence)))
