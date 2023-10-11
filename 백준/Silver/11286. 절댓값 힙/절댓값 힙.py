e3b5d107246b346e7f111a382619b00b538a4b10
JongWon Kim <127610150+BellOne4222@users.noreply.github.com>
2023-10-11 19:59:40 +0900

    10/11

--- 백준/Silver/11286. 절댓값 힙/절댓값 힙.py
+++ 백준/Silver/11286. 절댓값 힙/절댓값 힙.py
@@ -1,20 +1,24 @@
-import heapq
-import sys
+import heapq  # heapq 모듈을 임포트합니다. heapq는 최소 힙(min-heap)을 구현하는 데 사용됩니다.
+import sys  # sys 모듈을 임포트합니다. 이 모듈은 표준 입력(stdin)을 읽는 데 사용됩니다.
 
-arr = []
+arr = []  # 빈 리스트 'arr'을 생성합니다. 이 리스트는 최소 힙으로 활용됩니다.
 
-heapq.heapify(arr) # arr을 heapq로 변경
+heapq.heapify(arr)  # 'arr' 리스트를 최소 힙으로 변환합니다.
 
-n = int(sys.stdin.readline())
+n = int(sys.stdin.readline())  # 사용자로부터 입력을 받아 정수 'n'에 저장합니다. 'n'은 반복 횟수를 나타냅니다.
 
-for _ in range(n):
-    x = int(sys.stdin.readline())  
-    # x가 0이 아니면 heappush
-    if x != 0: 
-        heapq.heappush(arr,(abs(x),x)) # 우선 순위, 값형식의 튜플로 저장
-    # x가 0이면 heappop
-    else:
-        if arr:
+for _ in range(n):  # 'n'번 반복하는 루프를 시작합니다.
+    x = int(sys.stdin.readline())  # 사용자로부터 정수 'x'를 입력 받습니다.
+
+    if x != 0:  # 만약 'x'가 0이 아니라면 다음을 수행합니다.
+        heapq.heappush(arr, (abs(x), x))
+        # 'arr' 리스트에 (x의 절댓값, x) 형태의 튜플을 추가합니다. 
+        # 이때, 절댓값을 우선 순위로 사용하여 최소 힙을 유지합니다.
+
+    else:  # 'x'가 0이라면 다음을 수행합니다.
+        if arr:  # 'arr' 리스트에 원소가 존재한다면
             print(heapq.heappop(arr)[1])
-        else:
+            # 'arr'에서 최소값을 꺼내어 출력하고, 해당 튜플의 두 번째 요소인 'x'를 출력합니다.
+        else:  # 'arr' 리스트가 비어있다면
             print(0)
+            # 아무것도 출력하지 않고 0을 출력합니다.
--- 백준/Silver/2075. N번째 큰 수/N번째 큰 수.py
+++ 백준/Silver/2075. N번째 큰 수/N번째 큰 수.py
@@ -1,17 +1,20 @@
-import heapq
+import heapq  # heapq 모듈을 임포트합니다. heapq는 최소 힙(min-heap)을 구현하는 데 사용됩니다.
 
-n = int(input())
+n = int(input())  # 사용자로부터 정수 'n'을 입력 받습니다. 'n'은 입력할 숫자의 개수를 나타냅니다.
 
-arr = list(map(int,input().split()))
+arr = list(map(int, input().split()))
+# 사용자로부터 공백으로 구분된 숫자들을 입력 받아 리스트 'arr'에 저장합니다.
 
-heapq.heapify(arr) # 5 7 9 15 12
+heapq.heapify(arr)
+# 'arr' 리스트를 최소 힙으로 변환합니다. 이 때, 리스트 내의 원소들이 최소 힙 속성을 유지하도록 정렬됩니다.
 
-for _ in range(n-1):
-    lst = list(map(int,input().split()))
+for _ in range(n - 1):  # 'n-1'번 반복하는 루프를 시작합니다. (마지막 숫자는 결과값으로 출력할 것이므로 제외합니다)
+    lst = list(map(int, input().split()))
+    # 사용자로부터 공백으로 구분된 숫자들을 입력 받아 리스트 'lst'에 저장합니다.
 
-    for i in range(len(lst)):
-        if arr[0] < lst[i]:
-            heapq.heappop(arr)
-            heapq.heappush(arr,lst[i])
+    for i in range(len(lst)):  # 'lst'의 모든 원소에 대해 반복합니다.
+        if arr[0] < lst[i]:  # 'arr'의 최솟값(최소 힙의 루트)보다 'lst[i]'가 크다면
+            heapq.heappop(arr)  # 'arr'의 최솟값을 제거합니다.
+            heapq.heappush(arr, lst[i])  # 'lst[i]'를 'arr'에 추가합니다.
 
-print(arr[0])
+print(arr[0])  # 최종적으로 'arr'의 최솟값(최소 힙의 루트)을 출력합니다.
