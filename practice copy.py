428c7474bd4358597f7eaf9fd5c6c568a385a4d1
JongWon Kim <127610150+BellOne4222@users.noreply.github.com>
2023-10-11 19:49:03 +0900

    10/11

--- practice copy.py
+++ practice copy.py
@@ -1,22 +1,11 @@
-# 참고 : https://reliablecho-programming.tistory.com/102
+import heapq
 
-t = int(input())
+arr = [12,7,9,15,5]
 
-for _ in range(t):
-    l_list = []
-    r_list = []
-    data = input()
-    for i in data:
-        if i == '-':
-            if l_list: #왼쪽 스택에 있을 경우
-                l_list.pop()
-        elif i == '<': # 왼스택에 있는 문자 오른쪽 스택으로
-            if l_list:
-                r_list.append(l_list.pop())
-        elif i == '>': # 오스택에 있는 문자 왼쪽 스택으로
-            if r_list:
-                l_list.append(r_list.pop())
-        else:
-            l_list.append(i)
-    l_list.extend(reversed(r_list)) # 오른쪽 스택에 있는 문자들은 뒤집어서 붙여야 한다.
-    print(''.join(l_list))
+heapq.heapify(arr)
+
+heapq.heappush(arr,13)
+
+heapq.heappop(arr)
+
+print(arr)
--- practice.py
+++ practice.py
@@ -1,16 +1,14 @@
 import heapq
 import sys
 
-input = sys.stdin.readline()
-
 arr = []
 
 heapq.heapify(arr) # arr을 heapq로 변경
 
-n = int(input())
+n = int(sys.stdin.readline())
 
 for _ in range(n):
-    x = int(input())  
+    x = int(sys.stdin.readline())  
     # x가 0이 아니면 heappush
     if x != 0: 
         heapq.heappush(arr,(abs(x),x)) # 우선 순위, 값형식의 튜플로 저장
