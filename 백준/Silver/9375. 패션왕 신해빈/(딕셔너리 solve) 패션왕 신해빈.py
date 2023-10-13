eed5187f6bb62badd12bc4de82600be0f2595506
JongWon Kim <127610150+BellOne4222@users.noreply.github.com>
2023-10-13 16:58:46 +0900

    10/13

--- 백준/Silver/9375. 패션왕 신해빈/(딕셔너리 solve) 패션왕 신해빈.py
+++ 백준/Silver/9375. 패션왕 신해빈/(딕셔너리 solve) 패션왕 신해빈.py
@@ -15,6 +15,6 @@ for _ in range(t):
             clothes[kind] = 1
     case = 1
     for i in clothes:
-        case *= clothes[i] + 1 # 각 종류에 대한 항목을 사용하거나 미사용 경우
+        case *= clothes[i] + 1 # 각 종류에 대한 항목을 사용하거나 미사용 경우 -> hat 사용, turban 사용, 안쓰는 경우
     
-    print(case - 1)
+    print(case - 1) # 모두 벗는 경우(알몸) 제거
