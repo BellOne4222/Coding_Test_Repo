149f26d3b70e8d69e73c084f7ad4bdf7b30ff071
JongWon Kim <127610150+BellOne4222@users.noreply.github.com>
2023-10-13 16:56:28 +0900

    10/13

--- 백준/Silver/9375. 패션왕 신해빈/(Counter solve) 패션왕 신해빈.py
+++ 백준/Silver/9375. 패션왕 신해빈/(Counter solve) 패션왕 신해빈.py
@@ -1,18 +1,18 @@
 import sys
-from collections import Counter
 
 
 t = int(sys.stdin.readline())
 
 for _ in range(t):
     n = int(sys.stdin.readline())
-    clothes = []
+    clothes = {}
     for _ in range(n):
         name, kind = sys.stdin.readline().split()
-        clothes.append(kind)
-    
-    clothes = Counter(clothes)
 
+        if kind in clothes:
+            clothes[kind] += 1
+        else:
+            clothes[kind] = 1
     case = 1
     for i in clothes:
         case *= clothes[i] + 1 # 각 종류에 대한 항목을 사용하거나 미사용 경우
