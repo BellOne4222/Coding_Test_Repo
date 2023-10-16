0f69392e2bd98efa0d555332b6b137cd84c1cd7d
JongWon Kim <127610150+BellOne4222@users.noreply.github.com>
2023-10-16 20:02:30 +0900

    10/16

--- practice copy 4.py
+++ practice copy 4.py
@@ -1,19 +1,29 @@
-import sys
+# 함수 R은 배열에 있는 수의 순서를 뒤집는 함수이고, D는 첫 번째 수를 버리는 함수이다. 배열이 비어있는데 D를 사용한 경우에는 에러가 발생한다.
+# 함수는 조합해서 한 번에 사용할 수 있다. 예를 들어, "AB"는 A를 수행한 다음에 바로 이어서 B를 수행하는 함수이다. 예를 들어, "RDD"는 배열을 뒤집은 다음 처음 두 수를 버리는 함수이다.
 
-total = 0
-dic = dict()
-while 1:
-    word = sys.stdin.readline().rstrip()
-    if word == '':
-        break
-    total += 1   
-    if word in dic:   # 전에 이미 나왔으면
-        dic[word] += 1
-    else:
-        dic[word] = 1
-sdic = dict(sorted(dic.items()))
-for i in sdic:
-    a = sdic[i]
-    per = (a / total * 100)
-    
-    print("%s %.4f" %(i, per))
+import sys
+from collections import deque
+
+t = int(sys.stdin.readline())
+
+for _ in range(t):
+    operation = list(map(str,sys.stdin.readline().rstrip()))
+    n = int(sys.stdin.readline())
+    lst = deque(sys.stdin.readline().rstrip()[1:-1].split(","))
+    if n == 0:
+        lst = deque()
+
+    flag = True
+    for i in operation:
+        if i == 'R':
+            lst.reverse()
+        elif i == 'D':
+            if lst:
+                lst.popleft()
+            else:
+                print("error")
+                flag = False
+                break
+    if flag == 0:
+        print("["+",".join(lst)+"]")
+
