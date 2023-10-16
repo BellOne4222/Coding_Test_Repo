f24ac439b72dd3f01f90054029dc09bf07d2a8e1
JongWon Kim <127610150+BellOne4222@users.noreply.github.com>
2023-10-16 19:10:54 +0900

    10/16

--- practice copy 3.py
+++ practice copy 3.py
@@ -1,46 +1,13 @@
-# 반례는 모두 성공하지만 시간 초과...
+# 1. 창구수 설정
+# 2. 고객 별로 대기 번호를 받은 시각과 상담에 걸리는 시간을 설정
+# 3. 생성된 데이터를 이용해 상담 처리 과정을 시뮬레이션
+# if len(상담창구) == 0: 즉시 상담
+# not 빈 창구 : wait
+# end 상담시간 -> 빈창구 +=1
+# 전체 고객의 대기시간 합?
 
-import sys
+N = 2
+data = [[0, 3], [2, 5], [4, 2], [5, 3]]
 
-n = int(sys.stdin.readline())
-
-problems = {} # 난이도 : 문제번호
-reverse_problems = {} # 문제번호 : 난이도
-problems_levels = []
-
-for _ in range(n):
-    p, l = map(int, sys.stdin.readline().split()) # 문제 번호, 난이도
-    if l in problems:
-        problems[l].append(p)
-    else:
-        problems[l] = [p]
-    reverse_problems[p] = l
-    problems_levels.append(l)
-
-m = int(sys.stdin.readline())
-
-for _ in range(m):
-    operation = list(sys.stdin.readline().split())
-    # 추천 문제 리스트에 난이도가 L인 문제 번호 P를 추가한다.
-    if operation[0] == "add":
-        if int(operation[2]) in problems:
-            problems[int(operation[2])].append(int(operation[1]))
-        else:
-            problems[int(operation[2])] = [int(operation[1])]
-        reverse_problems[int(operation[1])] = int(operation[2])
-        problems_levels.append(int(operation[2]))
-    elif operation[0] == "recommend":
-        # 추천 문제 리스트에서 가장 어려운 문제의 번호를 출력한다.
-        if operation[1] == "1":
-            find = max(problems_levels)
-            print(max(problems[find]))
-        # x == -1인 경우 추천 문제 리스트에서 가장 쉬운 문제의 번호를 출력한다.
-        else:
-            find = min(problems_levels)
-            print(min(problems[find]))  
-    # solved인 경우 추천 문제 리스트에서 문제 번호 P를 제거한다.
-    else:
-        find = reverse_problems[int(operation[1])] # find = p의 난이도
-        problems[find].remove(int(operation[1]))   
-        del reverse_problems[int(operation[1])] # 문제번호에 해당하는 난이도 제거
-        problems_levels.remove(find) # 문제 번호에 해당하는 난이도 제거
+changgu = [[] for _ in range(N)]
+print(changgu)
--- practice copy.py
+++ practice copy.py
@@ -1,6 +1,89 @@
-arr = {}
+from collections import Counter
 
-arr[1] = [1000]
+products = ["towel red long thin", "blanket red thick short", "curtain red long wide", "mattress thick", "hat red thin", "pillow red long", "muffler blue thick long"]
+purchased = ["blanket", "curtain", "hat", "muffler"]
 
-arr[1].append(2000)
-print(arr)
+# 제품의 우선순위 = 각 제품의 특성으로 결정 -> 고객이 구매하지 않은 제품 중에서 우선순위가 가장 높은 하나를 선택해서 추천
+
+# 특성의 우선순위 => 고객이 구매했던 특성이 몇번 나타나는지 각각 세기 -> 특성 나타난 횟수 크기가 높은 순서대로 우선순위 -> 
+# 횟수가 같으면 사전순으로 앞서는게 우선순위(정렬 필요)
+
+# 제품의 우선순위 -> 더 높은 우선순위의 특성을 가지고 있는 제품 순서대로 우선순위
+# 가장 높은 운선순위 특성이 같아면 그 다음으로 우선순위가 높은 특성들을 차례대로 비교
+# 비교할 특성이 먼저 바닥난 제품의 우선순위가 상대적으로 낮다
+
+products_name = [] # 상품 이름 ['sofa', 'blanket', 'towel', 'mattress', 'curtain']
+products_dict = {} # 상품 이름 : 상품 특성 {'sofa': ['red', 'long'], 'blanket': ['blue', 'long'], 'towel': ['red'], 'mattress': ['long'], 'curtain': ['blue', 'long', 'cheap']}
+
+for i in range(len(products)):
+    sample = list(products[i].split())
+    products_name.append(sample[0])
+    products_dict[sample[0]] = []
+    for j in range(1,len(sample)):
+        products_dict[sample[0]].append(sample[j])
+
+customer_dict = {} # {'towel': ['red'], 'mattress': ['long'], 'curtain': ['blue', 'long', 'cheap']}
+for j in range(len(purchased)):
+    customer_dict[purchased[j]] = products_dict[purchased[j]]
+customer_cha = [] # ['red', 'long', 'blue', 'long', 'cheap']
+for k in customer_dict:
+    for l in range(len(customer_dict[k])):
+        customer_cha.append(customer_dict[k][l])
+
+counter_customer_cha = Counter(customer_cha) # Counter({'long': 2, 'red': 1, 'blue': 1, 'cheap': 1})
+cha_order = [] # [['long', 2], ['blue', 1], ['cheap', 1], ['red', 1]]
+
+for key,value in counter_customer_cha.items():
+    cha_order.append([key,value])
+
+cha_order = sorted(cha_order, key= lambda x:(-x[1],x[0]))
+
+order = [] # ['long', 'blue', 'cheap', 'red']
+for m in range(len(cha_order)):
+    order.append(cha_order[m][0])
+
+recommend_name_lst = [] # ['sofa', 'blanket']
+for n in range(len(products_name)):
+    if products_name[n] not in purchased:
+        recommend_name_lst.append(products_name[n])
+
+recommend_chr_lst = [] # [['towel', 'red', 'long', 'thin'], ['mattress', 'thick'], ['pillow', 'red', 'long']]
+for o in range(len(recommend_name_lst)):
+    recommend_chr = products_dict[recommend_name_lst[o]]
+    recommend_chr_lst.append([recommend_name_lst[o]])
+    for p in range(len(recommend_chr)):
+        recommend_chr_lst[o].append(recommend_chr[p])
+
+recommend_length_lst = []
+for q in range(len(recommend_chr_lst)):
+    recommend_length_lst.append(len(recommend_chr_lst[q]))
+
+length = max(recommend_length_lst)
+
+for r in range(1,length):
+    length = len(recommend_chr_lst)
+    recommend_all_lst = sorted(recommend_chr_lst, key= lambda x:(order.index(x[r])))
+    for s in range(length):
+        if not recommend_chr_lst[s]:
+            recommend_chr_lst.remove(recommend_chr_lst[s])
+        length = len(recommend_chr_lst)
+
+print(recommend_chr_lst)
+    
+
+
+
+
+
+            
+
+        
+
+
+
+
+
+
+
+
+
