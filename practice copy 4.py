67584b6ab74de95e0820d727f2a3ce2623f5b2b8
JongWon Kim <127610150+BellOne4222@users.noreply.github.com>
2023-10-17 18:12:21 +0900

    10/17

--- practice copy 4.py
+++ practice copy 4.py
@@ -1,29 +1,22 @@
-# 함수 R은 배열에 있는 수의 순서를 뒤집는 함수이고, D는 첫 번째 수를 버리는 함수이다. 배열이 비어있는데 D를 사용한 경우에는 에러가 발생한다.
-# 함수는 조합해서 한 번에 사용할 수 있다. 예를 들어, "AB"는 A를 수행한 다음에 바로 이어서 B를 수행하는 함수이다. 예를 들어, "RDD"는 배열을 뒤집은 다음 처음 두 수를 버리는 함수이다.
-
 import sys
-from collections import deque
 
-t = int(sys.stdin.readline())
+n = int(sys.stdin.readline().rstrip())
+p = ""
 
-for _ in range(t):
-    operation = list(map(str,sys.stdin.readline().rstrip()))
-    n = int(sys.stdin.readline())
-    lst = deque(sys.stdin.readline().rstrip()[1:-1].split(","))
-    if n == 0:
-        lst = deque()
+for i in range((2*n)+1):
+    if i%2 == 0:
+        p += "I"
+    else:
+        p += "O"
 
-    flag = True
-    for i in operation:
-        if i == 'R':
-            lst.reverse()
-        elif i == 'D':
-            if lst:
-                lst.popleft()
-            else:
-                print("error")
-                flag = False
-                break
-    if flag == 0:
-        print("["+",".join(lst)+"]")
+m = int(sys.stdin.readline().rstrip())
+s = sys.stdin.readline().rstrip()
+p_cnt = 0
+for i in range(m-len(p)+1):
+    compare = s[i:i+len(p)] 
+    if compare == p:
+        p_cnt += 1
+    
+    
 
+print(p_cnt)
