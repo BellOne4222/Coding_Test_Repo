b8a19d33572b4eff95e5f3ec94b336bca7d34358
JongWon Kim <127610150+BellOne4222@users.noreply.github.com>
2023-10-19 20:28:33 +0900

    [Gold V] Title: 동전 바꿔주기, Time: 4908 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Gold/2624. 동전 바꿔주기/동전 바꿔주기.py
+++ 백준/Gold/2624. 동전 바꿔주기/동전 바꿔주기.py
@@ -1,18 +1,21 @@
 import sys
-my_input = sys.stdin.readline
+input = sys.stdin.readline
 
-t = int(my_input())
-k = int(my_input())
-coins = [list(map(int, my_input().split())) for _ in range(k)]
+if __name__ == '__main__':
+    T = int(input()) # 지폐의 금액
+    K = int(input()) # 동전의 가지 수 
+    coins = [] # 동전 정보
+    dp = [0] * (T+1) # dp[n] : n 금액에 대한 동전 교환 방법의 가지 수 
+    dp[0] = 1 # 0원은 아무 동전도 사용하지 않는 경우가 하나 있으므로 1로 초기화 
 
-result = [0] * (t + 1)
-result[0] = 1
+    for _ in range(K):
+        p, n = map(int, input().split())
+        coins.append((p, n))
 
-for p, n in coins:
-	for i in range(t, 0, -1):
-		for j in range(1, n + 1):
-			answer = i - (p * j)
-			if answer >= 0:
-				result[i] += result[answer]
+    for coin, cnt in coins:
+        for money in range(T, 0, -1): # T원부터 1원까지 내려가며 진행 
+            for i in range(1, cnt+1): # 현재 동전 coin의 개수만큼 for문 진행 
+                if money - coin * i >= 0: # 0원 이상인 경우
+                    dp[money] += dp[money - coin * i]
 
-print(result[t])
+    print(dp[T])
