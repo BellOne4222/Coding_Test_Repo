41b0f57b5955bc3552f16a04e747b8473f872af3
JongWon Kim <127610150+BellOne4222@users.noreply.github.com>
2023-10-24 17:13:01 +0900

    10/24

--- practice copy.py
+++ practice copy.py
@@ -1,7 +1,13 @@
 import sys
 
-n,m = int(sys.stdin.readline().split())
+n,m = map(int,sys.stdin.readline().split())
 
-blueray = list(map(int,sys.stdin.readline()))
+blueray = list(map(int,sys.stdin.readline().split())) # [1, 2, 3, 4, 5, 6, 7, 8, 9]
 
-print(blueray)
+rng = n // m
+
+left = blueray[:rng]
+mid = blueray[rng:(n-rng)]
+right = blueray[(n-rng):]
+
+print(left, mid, right)
