d2747e216edfcdc65109d73f9011beb2b9f81f24
JongWon Kim <127610150+BellOne4222@users.noreply.github.com>
2023-10-24 16:13:03 +0900

    10/24

--- practice copy.py
+++ practice copy.py
@@ -1,30 +1,7 @@
 import sys
-import heapq
 
-n, m = map(int, sys.stdin.readline().rstrip().split())
+n,m = int(sys.stdin.readline().split())
 
-answer = []
-graph = [[] for _ in range(n + 1)]
-inDegree = [0 for _ in range(n+1)]
-queue = []
+blueray = list(map(int,sys.stdin.readline()))
 
-
-for i in range(m):
-    first, second = map(int, sys.stdin.readline().rstrip().split())
-    graph[first].append(second)
-    inDegree[second] += 1
-
-for i in range(1, n + 1):
-    if inDegree[i] == 0:
-        heapq.heappush(queue, i)
-
-while queue:
-    tmp = heapq.heappop(queue)
-    answer.append(tmp)
-    for i in graph[tmp]:
-        inDegree[i] -= 1
-        if inDegree[i] == 0:
-            heapq.heappush(queue, i)
-
-
-print(" ".join(map(str, answer)))
+print(blueray)
