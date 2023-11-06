dd52d6f1943ba18c83e8eb65f1dff3497babbdea
JongWon Kim <127610150+BellOne4222@users.noreply.github.com>
2023-11-06 17:17:12 +0900

    11/6

--- practice copy.py
+++ practice copy.py
@@ -1,16 +1,27 @@
+def Betal(fck):
+    if fck <= 28.0:
+        betal = 0.85
+    elif fck > 28.0:
+        betal = 0.85 - 0.007 * (fck - 28.0)
+        
+    if betal < 0.65:
+        betal = 0.65
+    return betal
+      
+
 # Strength Reduction Factor, et
-def et(et_value):
-   if et_value >= 0.005:
+def Strength_Reduction_Factor(et):
+   if et >= 0.005 :
       phi = 0.85
-   elif et_value < 0.002:
+   elif et < 0.002 :
       phi = 0.65
-   else:
-      phi = 0.65 + (et_value - 0.002) * (0.2 / 0.003)
+   else :
+      phi = 0.65 +( et - 0.002 ) * ( 0.2 / 0.003 )
    return phi
 
 
 
-def Dbeam(fy, fck, b, d, dt, As, Asp, dp):
+def Dbeam(fy, fck, b, d, dt, As, Asp, dp) :
    #design flexura1 strength of singly reinforced concrete beam (MPa,mm) 
 
    #return (pMn, Mn, phi, et, c, a)
@@ -23,24 +34,25 @@ def Dbeam(fy, fck, b, d, dt, As, Asp, dp):
    c = a / b1
    Es = 0.0033 * ( c - dp ) / c
 
-   if Es > 0.002 :
+   if Es > 0.002:
       Mn = ( Asp * fy * ( d - dp ) + ( As - Asp ) * fy * ( d - a/2)) * 1E-6 
-   else :
-    x = 0.85 * fck * b
-    y = 0.0033 * 200000 * Asp - As * fy
-    z = -0.0033 * 200000 * Asp * b1 * dp
-    a= (-y+(y**2 - 4 * x  * z)**0.5) / (2 * x)
-    fs = Es * 200000
-    Mn = ( 0.85 * fck * a * b *(d-a/2) + Asp * fs * (d- a/2) ) * 1E-6
+   else:
+      x = 0.85 * fck * b
+      y = 0.0033 * 200000 * Asp - As * fy
+      z = -0.0033 * 200000 * Asp * b1 * dp
+      a = (-y+(y**2 - 4 * x  * z)**0.5) / (2 * x)
+      fs = Es * 200000
+      Mn = ( 0.85 * fck * a * b *(d-a/2) + Asp * fs * (d- a/2) ) * 1E-6
 
     # check the extreme tensile strain(=et)
 
-    et = 0.0033 * ( dt-c ) / c
-    if et < 0.004 :
-       Mn = 0
+   et = 0.0033 * ( dt-c ) / c
+   if et < 0.004:
+      Mn = 0
+   
+   phi = Strength_Reduction_Factor(et)
 
    # design strength
-   phi = et
    pMn = phi * Mn
 
    #check minimum reinforcement
@@ -53,9 +65,9 @@ def Dbeam(fy, fck, b, d, dt, As, Asp, dp):
       Mn = pMn = 0
 
    # return analysis results
-   return(pMn, Mn, phi ,et, c, a)   
+   return pMn, Mn, phi ,et, c, a   
 
-# 예제 4-2
-fy, fck, b, d, dt, As, Asp, dp =  400, 24, 300, 515, 540, 60, 387.1 * 8, 387.1 * 2
-pMn, Mn, phi, et_value, c, a = Dbeam(fy, fck, b, d, dt, As, Asp, dp)
-print(f'[Dbeam] a={a:6.1f} mm, c={c:6.1f}mm, et={et_value: 7.4f}, Mn={Mn:6.1f}kN.m, phi={phi: 6.3f}, pMn={pMn:6.1f}kN.m')
+#예제 4-2
+fy, fck, b, d, dt, As, Asp, dp=  400, 24, 300, 515, 540, 60, 387.1 * 8, 387.1 *2
+pMn, Mn, phi, et, c, a = Dbeam(fy, fck, b, d, dt, As, Asp, dp)
+print(f'[Dbeam] a={a:6.1f} mm, c={c:6.1f}mm,et={et: 7.4f},Mn={Mn:6.1f}kN.m, phi={phi: 6.3f},pMn={pMn:6.1f}kN.m')
