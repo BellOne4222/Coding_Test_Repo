e3e868ee6d94869d2dc0eff47e187ce0cf077398
JongWon Kim <127610150+BellOne4222@users.noreply.github.com>
2023-11-09 19:46:26 +0900

    [Silver III] Title: N과 M (2), Time: 40 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Silver/15650. N과 M （2）/N과 M （2）.py
@@ -0,0 +1,18 @@
+import sys
+
+n,m = map(int, sys.stdin.readline().split())
+result = []
+
+def dfs(x):
+    if len(result) == m:
+        print(' '.join(map(str, result)))
+        return
+    
+    for i in range(x,n+1):
+        if i not in result:
+            result.append(i)
+            dfs(i+1)
+            result.pop()
+        
+
+dfs(1)
