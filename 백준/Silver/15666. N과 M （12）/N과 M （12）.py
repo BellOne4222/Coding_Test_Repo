6fcc993a3adc8b73c8c7a659196282a0fbdcf24d
JongWon Kim <127610150+BellOne4222@users.noreply.github.com>
2023-11-13 19:55:53 +0900

    11/13

--- 백준/Silver/15666. N과 M （12）/N과 M （12）.py
+++ 백준/Silver/15666. N과 M （12）/N과 M （12）.py
@@ -1,19 +1,28 @@
 # 비내림차순이다. -> 수열이 같거나 커지는 순이다. 정렬 이후 시작 index를 기준으로 같은 index이거나 더 큰 index만 탐색한다.
 
-n, m = map(int, input().split())
-nums = sorted(list(map(int, input().split())))
-temp = []
+import sys
 
-def dfs(start):
-    if len(temp) == m:
-        print(*temp)
+# 입력 받기
+n, m = map(int, sys.stdin.readline().split())
+numbers = list(map(int, sys.stdin.readline().split()))
+numbers.sort()
+result = []
+
+# 깊이 우선 탐색 함수 정의
+def dfs(depth):
+    # 길이가 M인 수열을 찾으면 출력하고 함수 종료
+    if len(result) == m:
+        print(' '.join(map(str, result)))
         return
-    remember_me = 0
-    for i in range(start, n):
-        if remember_me != nums[i]:
-            temp.append(nums[i])
-            remember_me = nums[i]
-            dfs(i)
-            temp.pop()
+    
+    compare = 0  # 중복된 수를 피하기 위한 변수
+    for i in range(depth, n):
+        # 중복된 수를 피하고 비내림차순 조건을 확인하여 수열에 추가
+        if compare != numbers[i]:
+            result.append(numbers[i])
+            compare = numbers[i]
+            dfs(i)  # 재귀 호출을 통해 다음 숫자를 선택
+            result.pop()  # 백트래킹을 위해 마지막에 선택한 수를 제거
 
+# 초기 호출
 dfs(0)
