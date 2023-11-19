2d702205b71e2a092c42f8f9d15d89d145c34ab2
JongWon Kim <127610150+BellOne4222@users.noreply.github.com>
2023-11-19 13:55:44 +0900

    [Silver II] Title: 태권왕, Time: 72 ms, Memory: 34028 KB -BaekjoonHub

---
+++ 백준/Silver/14562. 태권왕/태권왕.py
@@ -0,0 +1,24 @@
+import sys
+from collections import deque
+
+def bfs(s,t,kick):
+    q = deque()
+    q.append((s,t,kick))
+    
+    while q:
+        me, opponent, kicks = q.popleft()
+        
+        if me <= opponent:
+            q.append((me*2,opponent+3,kicks+1))
+            q.append((me+1,opponent,kicks+1))
+            
+            if me == opponent:
+                return kicks 
+    
+
+c = int(sys.stdin.readline())
+
+for _ in range(c):
+    kick = 0
+    s,t = map(int, sys.stdin.readline().split())
+    print(bfs(s,t,kick))
