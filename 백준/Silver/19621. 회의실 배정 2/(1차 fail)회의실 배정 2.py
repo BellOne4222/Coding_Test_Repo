eb3e85cde7a1607ed4f1baaf65ad3755430f9c8d
JongWon Kim <127610150+BellOne4222@users.noreply.github.com>
2023-11-27 16:40:05 +0900

    11/27

--- 백준/Silver/19621. 회의실 배정 2/(1차 fail)회의실 배정 2.py
+++ 백준/Silver/19621. 회의실 배정 2/(1차 fail)회의실 배정 2.py
@@ -1,4 +1,4 @@
-import sys
+import sys  # sys 모듈을 불러옵니다.
 
 n = int(sys.stdin.readline())
 
@@ -8,17 +8,32 @@ for _ in range(n):
     meeting = list(map(int, sys.stdin.readline().split()))
     meetings.append(meeting)
 
-# 회의의 시작 시간을 기준으로 정렬합니다.
-meetings.sort(key=lambda x: x[0])
+standard = meetings[-1][0]
 
-dp_table = [0] * n  # 각 회의까지의 최대 인원을 저장할 DP 테이블
+dp_table = [0] * (standard+1)
 
-for i in range(n):
+for i in reversed(range(len(meetings))):
     start, end, person = meetings[i][0], meetings[i][1], meetings[i][2]
-    dp_table[i] = person  # 초기값으로 각 회의의 인원을 넣어줍니다.
+    
+    dp_table[start] += person
+    
+    idx = meetings.index(meetings[i])
+    compare_lst = []
+    for j in range(idx, len(meetings)):
+        if meetings[j][0] >= end:
+            if dp_table[start] < dp_table[start] + dp_table[meetings[j][0]]:
+                compare_lst.append(dp_table[start] + dp_table[meetings[j][0]])
+    if compare_lst:
+        dp_table[start] = max(compare_lst)
 
-    for j in range(i):
-        if meetings[j][1] <= start:  # 이전 회의가 현재 회의 시작 시간보다 끝나는 시간이 작거나 같은 경우
-            dp_table[i] = max(dp_table[i], dp_table[j] + person)  # 현재 회의까지의 최대 인원을 구합니다.
-
-print(max(dp_table))  # 최대 인원을 출력합니다.
+print(max(dp_table))
+            
+        
+        
+        
+        
+        
+            
+            
+        
+    
