2ca744547f98ad12d5fc5dc3b4f1126df4be4501
JongWon Kim <127610150+BellOne4222@users.noreply.github.com>
2023-11-28 20:55:48 +0900

    11/28

--- practice copy.py
+++ practice copy.py
@@ -1,39 +1,46 @@
 import sys  # sys 모듈을 불러옵니다.
+from collections import deque
 
-n = int(sys.stdin.readline())
 
-meetings = []
-
-for _ in range(n):
-    meeting = list(map(int, sys.stdin.readline().split()))
-    meetings.append(meeting)
-
-standard = meetings[-1][0]
-
-dp_table = [0] * (standard+1)
-
-for i in reversed(range(len(meetings))):
-    start, end, person = meetings[i][0], meetings[i][1], meetings[i][2]
+def bfs(x,y,visited):
     
-    dp_table[start] += person
-    
-    idx = meetings.index(meetings[i])
-    compare_lst = []
-    for j in range(idx, len(meetings)):
-        if meetings[j][0] >= end:
-            if dp_table[start] < dp_table[start] + dp_table[meetings[j][0]]:
-                compare_lst.append(dp_table[start] + dp_table[meetings[j][0]])
-    if compare_lst:
-        dp_table[start] = max(compare_lst)
+    dx = [0,0,1,-1]
+    dy = [1,-1,0,0]
 
-print(max(dp_table))
-            
-        
-        
-        
-        
-        
-            
-            
-        
+    queue = deque()
+    queue.append([x,y])
+    visited[x][y] = True
+    trash = 0
     
+    while queue:
+        cur_x, cur_y = queue.popleft()
+        trash += 1
+        for i in range(4):
+            nx = cur_x + dx[i]
+            ny = cur_y + dy[i]
+            
+            if 0 <= nx < n and 0 <= ny < m and not visited[nx][ny] and path[nx][ny]:
+                visited[nx][ny] = True
+                queue.append([nx,ny])
+    
+    return trash
+                
+
+n,m,k = map(int, sys.stdin.readline().split())
+
+path = [[0] * m for _ in range(n)]
+visited = [[0] * m for _ in range(n)]
+max_trash = 0
+
+for _ in range(k):
+    r,c = map(int, sys.stdin.readline().split())
+    path[r-1][c-1] = 1
+    
+for i in range(n):
+    for j in range(m):
+        if not visited[i][j] and path[i][j]:
+            answer = bfs(i,j,visited)
+            max_trash = max(max_trash, answer)
+                
+
+print(max_trash)
--- 백준/Silver/1743. 음식물 피하기/음식물 피하기.py
+++ 백준/Silver/1743. 음식물 피하기/음식물 피하기.py
@@ -1,39 +1,50 @@
-import sys
+import sys  # sys 모듈을 불러옵니다.
 from collections import deque
 
-input = sys.stdin.readline
-dx = [0, 0, -1, 1]
-dy = [-1, 1, 0, 0]
+# BFS 함수 정의
+def bfs(x, y, visited):
+    # 상하좌우 방향을 나타내는 리스트
+    dx = [0, 0, 1, -1]
+    dy = [1, -1, 0, 0]
 
+    # 큐 생성 및 시작 위치 초기화
+    queue = deque()
+    queue.append([x, y])
+    visited[x][y] = True  # 방문한 곳 표시
+    trash = 0  # 쓰레기 크기를 나타내는 변수 초기화
+    
+    # BFS 탐색
+    while queue:
+        cur_x, cur_y = queue.popleft()
+        trash += 1  # 쓰레기 크기 1 증가
+        for i in range(4):
+            nx = cur_x + dx[i]
+            ny = cur_y + dy[i]
+            
+            # 인덱스가 범위 내에 있고, 방문하지 않았으며, 쓰레기가 있는 경우
+            if 0 <= nx < n and 0 <= ny < m and not visited[nx][ny] and path[nx][ny]:
+                visited[nx][ny] = True  # 방문한 곳으로 표시
+                queue.append([nx, ny])  # 큐에 새로운 위치 추가
+    
+    return trash  # 최종 쓰레기 크기 반환
 
-def bfs(i, j, trash):
-    q = deque([[i, j]])
-    trash[i][j] = 2  # visited
-    result = 1
+# 입력 처리
+n, m, k = map(int, sys.stdin.readline().split())
+path = [[0] * m for _ in range(n)]  # 쓰레기 위치를 나타내는 리스트 초기화
+visited = [[0] * m for _ in range(n)]  # 방문 여부를 나타내는 리스트 초기화
+max_trash = 0  # 최대 쓰레기 크기 초기화
 
-    while q:
-        x, y = q.popleft()
-
-        for d in range(4):
-            nx, ny = x + dx[d], y + dy[d]
-            if 0 < nx <= n and 0 < ny <= m and trash[nx][ny] == 1:
-                q.append([nx, ny])
-                trash[nx][ny] = 2
-                result += 1
-    return result
-
-
-n, m, k = map(int, input().split())
-trash = [[0] * (m + 1) for _ in range(n + 1)]
-answer = 0
+# 쓰레기 위치 정보 입력 및 처리
 for _ in range(k):
-    x, y = map(int, input().split())
-    trash[x][y] = 1
+    r, c = map(int, sys.stdin.readline().split())
+    path[r - 1][c - 1] = 1  # 쓰레기가 있는 위치 표시
 
-for i in range(1, n + 1):
-    for j in range(1, m + 1):
-        if trash[i][j] == 1:
-            ans = bfs(i, j, trash)
-            answer = max(ans, answer)
+# 모든 위치에 대해 방문하지 않았고 쓰레기가 있는 곳을 탐색하여 최대 쓰레기 크기 찾기
+for i in range(n):
+    for j in range(m):
+        if not visited[i][j] and path[i][j]:
+            answer = bfs(i, j, visited)
+            max_trash = max(max_trash, answer)
 
-print(answer)
+# 최대 쓰레기 크기 출력
+print(max_trash)
