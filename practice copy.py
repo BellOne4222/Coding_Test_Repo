5c87c942ff0d4331a89f5fb2178bd4b5453dcae8
JongWon Kim <127610150+BellOne4222@users.noreply.github.com>
2023-11-30 19:40:34 +0900

    11/30

--- practice copy.py
+++ practice copy.py
@@ -1,46 +1,35 @@
 import sys  # sys 모듈을 불러옵니다.
 from collections import deque
 
+n,m,k,x = map(int, sys.stdin.readline().split())
 
-def bfs(x,y,visited):
+graph = [[] for _ in range(n+1)]
+visited = [False] * (n+1)
+paths = [0] * (n+1)
+
+for _ in range(n):
+    start, end = map(int, sys.stdin.readline().split())
+    graph[start].append(end)
+    graph[end].append(start)
+
+vertex = []
+queue = deque()
+queue.append(x)
+visited[x] = True
     
-    dx = [0,0,1,-1]
-    dy = [1,-1,0,0]
+while queue:
+    v = queue.popleft()
+    for i in graph[v]:
+        if not visited[i]:
+            visited[i] = True
+            queue.append(i)
+            paths[i] = paths[v] + 1
+            if paths[i] == k:
+                vertex.append(i)
 
-    queue = deque()
-    queue.append([x,y])
-    visited[x][y] = True
-    trash = 0
-    
-    while queue:
-        cur_x, cur_y = queue.popleft()
-        trash += 1
-        for i in range(4):
-            nx = cur_x + dx[i]
-            ny = cur_y + dy[i]
-            
-            if 0 <= nx < n and 0 <= ny < m and not visited[nx][ny] and path[nx][ny]:
-                visited[nx][ny] = True
-                queue.append([nx,ny])
-    
-    return trash
-                
-
-n,m,k = map(int, sys.stdin.readline().split())
-
-path = [[0] * m for _ in range(n)]
-visited = [[0] * m for _ in range(n)]
-max_trash = 0
-
-for _ in range(k):
-    r,c = map(int, sys.stdin.readline().split())
-    path[r-1][c-1] = 1
-    
-for i in range(n):
-    for j in range(m):
-        if not visited[i][j] and path[i][j]:
-            answer = bfs(i,j,visited)
-            max_trash = max(max_trash, answer)
-                
-
-print(max_trash)
+if len(vertex) == 0:
+    print(-1)
+else:
+    vertex.sort()
+    for v in vertex:
+        print(v, end = "\n")
