2a40c0444d49fb90c7bc90a77982b7a82bbb19eb
JongWon Kim <127610150+BellOne4222@users.noreply.github.com>
2023-11-30 19:58:40 +0900

    11/30

--- practice copy.py
+++ practice copy.py
@@ -1,35 +1,17 @@
 import sys  # sys 모듈을 불러옵니다.
 from collections import deque
 
-n,m,k,x = map(int, sys.stdin.readline().split())
+p = int(sys.stdin.readline())
 
-graph = [[] for _ in range(n+1)]
-visited = [False] * (n+1)
-paths = [0] * (n+1)
+board = [['*' for _ in range(3)] for _ in range(3)]
 
-for _ in range(n):
-    start, end = map(int, sys.stdin.readline().split())
-    graph[start].append(end)
-    graph[end].append(start)
+print(board)
 
-vertex = []
-queue = deque()
-queue.append(x)
-visited[x] = True
+for i in range(p):
+    right_board = []
+    for j in range(3):
+        line = list(map(str, sys.stdin.readline().rstrip()))
+        right_board.append(line)
     
-while queue:
-    v = queue.popleft()
-    for i in graph[v]:
-        if not visited[i]:
-            visited[i] = True
-            queue.append(i)
-            paths[i] = paths[v] + 1
-            if paths[i] == k:
-                vertex.append(i)
+    
 
-if len(vertex) == 0:
-    print(-1)
-else:
-    vertex.sort()
-    for v in vertex:
-        print(v, end = "\n")
