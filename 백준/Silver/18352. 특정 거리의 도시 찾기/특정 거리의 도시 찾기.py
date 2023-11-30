d99b4b753c2a3126bd965725a27a124f4b0ad2cc
JongWon Kim <127610150+BellOne4222@users.noreply.github.com>
2023-11-30 17:20:10 +0900

    [Silver II] Title: 특정 거리의 도시 찾기, Time: 1816 ms, Memory: 101120 KB -BaekjoonHub

--- 백준/Silver/18352. 특정 거리의 도시 찾기/특정 거리의 도시 찾기.py
+++ 백준/Silver/18352. 특정 거리의 도시 찾기/특정 거리의 도시 찾기.py
@@ -1,39 +1,34 @@
+import sys  # sys 모듈을 불러옵니다.
 from collections import deque
-import sys
-input = sys.stdin.readline
-sys.setrecursionlimit(10**6)
 
-n, m, k, x = map(int,input().split())
+n,m,k,x = map(int, sys.stdin.readline().split())
 
 graph = [[] for _ in range(n+1)]
+visited = [False] * (n+1)
 paths = [0] * (n+1)
 
 for _ in range(m):
-    s, e = map(int,input().split())
-    graph[s].append(e)
+    start, end = map(int, sys.stdin.readline().split())
+    graph[start].append(end)
 
-visited = [False] * (n+1)
-
-def bfs(start):
-    vertex = []
-    queue = deque([start])
-    visited[start] = True
-    while queue:
-        v = queue.popleft()
-        for i in graph[v]:
-            if not visited[i]:
-                visited[i] = True
-                queue.append(i)
-                paths[i] = paths[v] + 1
-                if paths[i] == k:
-                    vertex.append(i)
-    if len(vertex) == 0:
-        print(-1)
-    else:
-        vertex.sort()
-        for ve in vertex:
-            print(ve, end='\n')
-                
+vertex = []
+queue = deque()
+queue.append(x)
+visited[x] = True
     
+while queue:
+    v = queue.popleft()
+    for i in graph[v]:
+        if not visited[i]:
+            visited[i] = True
+            queue.append(i)
+            paths[i] = paths[v] + 1
+            if paths[i] == k:
+                vertex.append(i)
 
-bfs(x)
+if len(vertex) == 0:
+    print(-1)
+else:
+    vertex.sort()
+    for v in vertex:
+        print(v, end = "\n")
