b8eb3fd4bfd7d46c52399e17f6e1576f950d39a4
JongWon Kim <127610150+BellOne4222@users.noreply.github.com>
2023-12-04 17:43:56 +0900

    12/4

--- practice copy 2.py
+++ practice copy 2.py
@@ -1,31 +1,70 @@
 from collections import deque
+dx = [0, 0, -1, 1]
+dy = [1, -1, 0, 0]
+field_info = []
 
-n, m, k = map(int, input().split())
-arr = [[0]*m for _ in range(n)]
+for _ in range(12):
+    field_info.append(list(input()))
 
-for _ in range(k):
-    a, b = map(int, input().split())
-    arr[a-1][b-1] = 1
+def bfs(a, b, c):
+    global boom_flag
+    boom_list = []
+    deq = deque()
 
-direct = [(1, 0), (-1, 0), (0, 1), (0, -1)]
-visit = [[0]*m for _ in range(n)]
-ans = 0
+    deq.append([a,b])
+    boom_list.append([a,b])
 
-for r in range(n):
-    for c in range(m):
-        if arr[r][c] and not visit[r][c]:
-            Q = deque([(r, c)])
-            cnt = 0
-            visit[r][c] = 1
+    field_check[a][b] = True
 
-            while Q:
-                nr, nc = Q.popleft()
-                cnt += 1
-                for d in direct:
-                    sr, sc = nr+d[0], nc+d[1]
-                    if 0 <= sr < n and 0 <= sc < m and not visit[sr][sc] and arr[sr][sc]:
-                        visit[sr][sc] = 1
-                        Q.append((sr, sc))
-            ans = max(ans, cnt)
+    n = 1
 
-print(ans)
+    while deq:
+        x, y = deq.popleft()
+
+        for i in range(4):
+            nx, ny = x + dx[i], y + dy[i]
+            if 0 <= nx < 12 and 0 <= ny < 6:
+                if field_info[nx][ny] == c and not field_check[nx][ny]:
+                    field_check[nx][ny] = True
+                    deq.append([nx,ny])
+                    boom_list.append([nx,ny])
+                    n += 1
+
+    if n >= 4:
+        for b in boom_list:
+            field_info[b[0]][b[1]] = '.'
+
+        boom_flag = True
+
+boom_count = 0
+
+while True:
+    boom_flag = False
+
+    field_check = [[False] * 6 for _ in range(12)]
+
+    for i in range(12):
+        for j in range(6):
+            if field_info[i][j] != '.':
+                bfs(i,j,field_info[i][j])
+
+
+    for i in range(6):
+        rotate_queue = deque()
+
+        for j in range(11,-1,-1):
+            if field_info[j][i] != '.':
+                rotate_queue.append(field_info[j][i])
+
+        for j in range(11,-1,-1):
+            if rotate_queue:
+                field_info[j][i] = rotate_queue.popleft()
+            else:
+                field_info[j][i] = '.'
+
+    if not boom_flag:
+        break
+    else:
+        boom_count += 1
+
+print(boom_count)
