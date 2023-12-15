55be29dc2b4256a0cb83248fa5533012e9272ec3
JongWon Kim <127610150+BellOne4222@users.noreply.github.com>
2023-12-15 19:07:21 +0900

    12/15

--- practice copy.py
+++ practice copy.py
@@ -1,6 +1,20 @@
-import sys  # sys 모듈을 불러옵니다.
+# https://velog.io/@soobin519/Python-%EB%B0%B1%EC%A4%80-2631-%EC%A4%84%EC%84%B8%EC%9A%B0%EA%B8%B0
 
-s = sys.stdin.readline()
-p = sys.stdin.readline()
+import sys
 
-print(s,p)
+
+n = int(sys.stdin().readline())
+
+dp_table = [1] * (n+1)
+nums = [0]
+for i in range(n):
+    nums.append(int(sys.stdin().readline()))
+
+#가장 긴 증가하는 수열 찾기 
+for i in range(1,n+1):
+    for j in range(1,i):
+        if nums[j] < nums[i]:
+            dp_table[i]=max(dp_table[i],dp_table[j]+1)
+
+#n- 긴 증가하는 부분수열의 길이 
+print(n-max(dp_table))
--- 백준/Gold/2631. 줄세우기/줄세우기.py
+++ 백준/Gold/2631. 줄세우기/줄세우기.py
@@ -1,18 +1,22 @@
+# 참고 : https://velog.io/@soobin519/Python-%EB%B0%B1%EC%A4%80-2631-%EC%A4%84%EC%84%B8%EC%9A%B0%EA%B8%B0
+
 import sys
 
+n = int(sys.stdin.readline())  # 아이들의 수 입력 받기
 
-n = int(sys.stdin.readline())
+dp_table = [1] * (n+1)  # 아이들이 번호 순서대로 정렬되는데 필요한 최소 이동 수를 저장할 DP 테이블
+nums = [0]  # 입력된 아이들의 번호를 저장할 리스트, 0번째는 더미값
 
-dp_table = [1] * (n+1)
-nums = [0]
+# 각 아이들의 번호 입력 받기
 for i in range(n):
     nums.append(int(sys.stdin.readline()))
 
-#가장 긴 증가하는 수열 찾기 
-for i in range(1,n+1):
-    for j in range(1,i):
+# 가장 긴 증가하는 부분 수열(LIS)을 찾는 과정
+for i in range(1, n+1):
+    for j in range(1, i):
         if nums[j] < nums[i]:
-            dp_table[i]=max(dp_table[i],dp_table[j]+1)
+            dp_table[i] = max(dp_table[i], dp_table[j]+1)
 
-#n- 긴 증가하는 부분수열의 길이 
-print(n-max(dp_table))
+# 번호 순서대로 정렬하기 위해 옮겨지는 아이들의 최소 수 계산
+# 전체 아이들의 수에서 가장 긴 증가하는 부분 수열의 길이를 뺀 값이 옮겨지는 아이들의 최소 수
+print(n - max(dp_table))
