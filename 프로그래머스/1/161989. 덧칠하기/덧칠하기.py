14a424b962c59236a5ab9b76e7b09d187bcd00a5
JongWon Kim <127610150+BellOne4222@users.noreply.github.com>
2024-01-15 18:54:43 +0900

    1/15

--- 프로그래머스/1/161989. 덧칠하기/덧칠하기.py
+++ 프로그래머스/1/161989. 덧칠하기/덧칠하기.py
@@ -1,9 +1,10 @@
 def solution(n, m, section):
     
+    # 처음 롤러 생성
     roller = 1
     
     roller_first = section[0]
-    roller_end = roller_first + m -1
+    roller_end = roller_first + m - 1
     
     for i in range(len(section)):
         if roller_first <= section[i] <= roller_end:
