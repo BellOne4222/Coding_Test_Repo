9e6bb8d11bad81588dad51dfb10d3644db7372ed
JongWon Kim <127610150+BellOne4222@users.noreply.github.com>
2024-01-23 12:45:53 +0900

    [level 2] Title: 롤케이크 자르기, Time: 645.43 ms, Memory: 51.3 MB -BaekjoonHub

---
+++ 프로그래머스/2/132265. 롤케이크 자르기/롤케이크 자르기.py
@@ -0,0 +1,42 @@
+from collections import Counter
+
+def solution(topping):
+    # 정답을 저장할 변수 초기화
+    answer = 0
+    
+    # 철수가 가진 토핑의 개수를 세는 Counter 객체 생성
+    chulsu = Counter(topping)
+    
+    # 동생이 가진 토핑을 저장할 집합 초기화
+    brother = set()
+    
+    # 각 토핑에 대해 반복
+    for t in topping:
+        # 철수의 토핑 개수 감소
+        chulsu[t] -= 1
+        
+        # 동생이 가진 토핑 집합에 추가
+        brother.add(t)
+        
+        # 만약 철수가 해당 토핑을 모두 사용했다면 Counter에서 제거
+        if chulsu[t] == 0:
+            chulsu.pop(t)
+            
+        # 현재까지 철수와 동생이 가진 토핑의 종류가 같다면 정답 증가
+        if len(chulsu) == len(brother):
+            answer += 1
+            
+    # 최종적으로 정답 반환
+    return answer
+
+# Counter 객체 생성: Counter를 사용하여 topping 배열의 각 토핑별 개수를 셉니다. 이를 통해 철수가 가진 토핑의 개수를 파악합니다.
+
+# 반복문: topping 배열을 순회하면서 각 토핑에 대한 처리를 수행합니다.
+
+# 철수의 토핑 개수를 1 감소시키고, 동생이 가진 토핑을 집합에 추가합니다.
+
+# 만약 철수가 해당 토핑을 모두 사용했다면 Counter에서 해당 토핑을 제거합니다.
+
+# 현재까지 철수와 동생이 가진 토핑의 종류가 같다면 정답을 증가시킵니다.
+
+# 최종적으로 정답을 반환합니다.
