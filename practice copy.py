c09b6637168802e583badcf060110aa920f9d207
JongWon Kim <127610150+BellOne4222@users.noreply.github.com>
2024-01-26 13:03:17 +0900

    1/26

--- practice copy.py
+++ practice copy.py
@@ -1,15 +1,49 @@
-n = 11
 
-nums = ["1", "2", "4"]
 
-ans = ""
+m, n, board= 4,	5,	["CCBDE", "AAADE", "AAABF", "CCBBF"]
 
-while n > 0:
-    n = n - 1
-    ans = nums[n%3] + ans
-    n //= 3
-
-print(ans)
+answer = 0
+board = list(map(list, board))
     
+while True:
+    # temp = check(m, n, board)
     
+    filter = [[0 for _ in range(n)] for _ in range(m)]
+    count = 0
     
+    for i in range(m-1):
+        for j in range(n-1):
+            a = board[i][j]
+            b = board[i][j+1]
+            c = board[i+1][j]
+            d = board[i+1][j+1]
+            if a == b == c == d and a != '0':
+                filter[i][j], filter[i][j+1], filter[i+1][j], filter[i+1][j+1] = 1, 1, 1, 1
+    
+    for i in range(m):
+        for j in range(n):
+            if filter[i][j] == 1:
+                count += 1
+                board[i][j] = '0'
+    
+    if count == 0:
+        temp = 0
+    
+    for i in range(m-2, -1, -1):
+        for j in range(n):
+            k = i
+            while 0 <= k+1 < m and board[k+1][j] == '0':
+                k += 1
+            if k != i:
+                board[k][j] = board[i][j]
+                board[i][j] = '0'
+    
+    temp = count
+    
+    if temp == 0:
+        break
+    answer += temp
+        
+print(answer)
+
+# print(solution(4,	5,	["CCBDE", "AAADE", "AAABF", "CCBBF"]))
