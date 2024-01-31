9235b555046968c35f2fbaaebe8f3d5856272dd6
JongWon Kim <127610150+BellOne4222@users.noreply.github.com>
2024-01-31 19:21:37 +0900

    1/31

--- practice copy 2.py
+++ practice copy 2.py
@@ -1,76 +1,23 @@
 # cap, n, deliveries, pickups = 2,7,[1, 0, 2, 0, 1, 0, 2], [0, 2, 0, 1, 0, 2, 0]	
 cap, n, deliveries, pickups = 4,	5,	[1, 0, 3, 1, 2],	[0, 3, 0, 4, 0]
 
-total_distance = 0
+deliveries = deliveries[::-1]  # 배열을 뒤집어서 뒤에서부터 처리하기 위해
+pickups = pickups[::-1]        # 배열을 뒤집어서 뒤에서부터 처리하기 위해
+answer = 0
 
-while True:
-    cur_cap = cap
-    delivery_distance = 0
-    delivery_chk = False
-    last_delivery = 0
-    pickup_distance = 0
-    pickup_chk = False
-    
-    # 배달
-    if not all(element == 0 for element in deliveries):
-        for i in reversed(range(n)):
-        # 배달 할 물건이 있을 떄
-            if deliveries[i] != 0:
-                # 집에 배달 할 수 있는 택배의 수를 모두 배달 할 수 있는 경우
-                if cur_cap - deliveries[i] >= 0:
-                    cur_cap -= deliveries[i] # 택배 모두 배송
-                    if not delivery_chk: # 제일 끝 배달한 집 주소 저장
-                        delivery_chk = True
-                        delivery_distance += i+1
-                        last_delivery = i+1
-                    deliveries[i] = 0
-                    
-                
-                # 집에 배달 할 수 있는 택배의 수를 모두 배달 할 수 없는 경우
-                else:
-                    if cur_cap != 0:
-                        deliveries[i] = deliveries[i] - cur_cap # 부분 배달 처리
-                        break
-                # 트럭이 가득차면 배달 종료
-                if cur_cap == 0:
-                    break
-        
-    
-        
-    if not all(element == 0 for element in pickups):
-        # 수거
-        for j in reversed(range(n)):
-            # 수거 할 물건이 있을 때
-            if pickups[j] != 0:
-                # 집에 있는 박스를 모두 수거 할 수 있는 경우
-                if cur_cap + pickups[j] <= cap:
-                    cur_cap += pickups[j] # 수거
-                    pickups[j] = 0
-                    
-                    if not pickup_chk:
-                        pickup_chk = True
-                        last_pickup = j+1 
-                    
-                    # 배송한 가장 끝집부터 수거 하므로 배송한 가장 마지막 지점부터 수거 거리 계산
-                    if last_delivery != 0:
-                        pickup_distance += last_delivery
-                    # 배송을 안하고 수거만 한 경우
-                    else:
-                        pickup_distance += last_pickup
-                # 부분 수거
-                else:
-                    if cur_cap < cap:
-                        pickups[j] = pickups[j] - (cap - cur_cap)
-                        
-                
-    
-    total_distance += delivery_distance
-    total_distance += pickup_distance
-        
-    if all(element == 0 for element in deliveries) and all(element == 0 for element in pickups):
-        break
+have_to_deli = 0  # 현재 집까지 배달해야 하는 택배 상자의 개수
+have_to_pick = 0  # 현재 집까지 수거해야 하는 빈 택배 상자의 개수
 
-print(total_distance)
+for i in range(n):
+    have_to_deli += deliveries[i]  # 현재 집까지의 배달해야 하는 택배 상자의 개수 갱신
+    have_to_pick += pickups[i]     # 현재 집까지의 수거해야 하는 빈 택배 상자의 개수 갱신
+
+    while have_to_deli > 0 or have_to_pick > 0:  # 아직 배달이나 수거할 택배가 남아있을 경우
+        have_to_deli -= cap  # 트럭에 실린 만큼 배달해야 하는 택배 개수 갱신
+        have_to_pick -= cap  # 트럭에 실린 만큼 수거해야 하는 빈 택배 상자 개수 갱신
+        answer += (n - i) * 2  # 현재 집에서 물류창고까지의 이동 거리를 더함
+
+print(answer)
                     
                         
                     
