eb394be6f36fdd510463f28f8f040399a1251a30
JongWon Kim <127610150+BellOne4222@users.noreply.github.com>
2024-02-19 19:30:57 +0900

    2/19

--- practice copy.py
+++ practice copy.py
@@ -1,33 +1,39 @@
+
+
 import sys
 from collections import deque
 
 def bfs(x, y, z):
-    
+    # 상하좌우, 위아래를 이동하기 위한 배열
     dx = [0, 0, 0, 0, 1, -1]
     dy = [0, 0, 1, -1, 0, 0]
     dz = [1, -1, 0, 0, 0, 0]
     
+    # 큐 생성 및 시작점 추가
     queue = deque()
     queue.append((x, y, z, 0))
     
+    # BFS 탐색
     while queue:
         cur_x, cur_y, cur_z, cur_time = queue.popleft()
         
+        # 현재 위치에서 6방향으로 이동
         for i in range(6):
             nx = cur_x + dx[i]
             ny = cur_y + dy[i]
             nz = cur_z + dz[i]
             
+            # 상범 빌딩의 범위 내에 있고, 방문하지 않았고, 이동 가능한 공간인 경우
             if 0 <= nx < l and 0 <= ny < r and 0 <= nz < c:
                 if visited[nx][ny][nz] == False and buildings[nx][ny][nz] == '.':
                     visited[nx][ny][nz] = True
                     queue.append((nx, ny, nz, cur_time + 1))
+                # 출구에 도착한 경우
                 elif buildings[nx][ny][nz] == 'E' and visited[nx][ny][nz] == False:
                     return 'Escaped in ' + str(cur_time + 1) + ' minute(s).'
     
+    # 탈출 불가능한 경우
     return 'Trapped!'
-                
-        
 
 while True:
     l, r, c = map(int, sys.stdin.readline().split())
@@ -35,20 +41,21 @@ while True:
         break
     
     buildings = []
+    # 빌딩 정보 입력
     for i in range(l):
         buildings.append([list(sys.stdin.readline().strip()) for _ in range(r)])
+        # 각 층 사이 빈 줄 입력
         sys.stdin.readline()
         
-    
+    # 방문 여부를 저장할 배열 초기화
     visited = [[[False] * c for _ in range(r)] for _ in range(l)]
     
+    # 시작 지점 탐색
     for j in range(l):
         for k in range(r):
             for m in range(c):
                 if buildings[j][k][m] == 'S':
                     visited[j][k][m] = True
+                    # BFS 호출하여 결과 출력
                     print(bfs(j, k, m))
-                    break    
-    
-    
-    
+                    break
--- 백준/Silver/7562. 나이트의 이동/나이트의 이동.py
+++ 백준/Silver/7562. 나이트의 이동/나이트의 이동.py
@@ -4,8 +4,8 @@ from collections import deque
 def bfs(x, y, end_x, end_y):
     
     # 나이트가 이동 할 수 있는 8가지 방향
-    dx = [-1,1,2,2,1,-1,-2,-2]
-    dy = [2,2,1,-1,-2,-2,-1,1]
+    dx = [-1, 1, 2, 2, 1, -1, -2, -2]
+    dy = [2, 2, 1, -1, -2, -2, -1, 1]
     
     queue = deque()
     queue.append((x, y))
@@ -14,7 +14,7 @@ def bfs(x, y, end_x, end_y):
         cur_x, cur_y = queue.popleft()
         
         if cur_x == end_x and cur_y == end_y:
-            return visited[cur_x][cur_y] - 1
+            return visited[cur_x][cur_y] - 1  # 이동 횟수 반환
         
         for i in range(8):
             nx = cur_x + dx[i]
@@ -23,21 +23,16 @@ def bfs(x, y, end_x, end_y):
             if 0 <= nx < l and 0 <= ny < l:
                 if visited[nx][ny] == 0:
                     queue.append((nx, ny))
-                    visited[nx][ny] = visited[cur_x][cur_y] + 1
-                    
-                
-        
-    
+                    visited[nx][ny] = visited[cur_x][cur_y] + 1  # 방문 여부와 이동 횟수 업데이트
 
-t = int(sys.stdin.readline()) # 테스트 케이스의 개수
-
+t = int(sys.stdin.readline())  # 테스트 케이스의 개수
 
 for i in range(t):
-    l = int(sys.stdin.readline()) # 체스판의 한변의 길이
-    start_x, start_y = map(int, sys.stdin.readline().split()) # 나이트의 현재 x,y좌표
+    l = int(sys.stdin.readline())  # 체스판의 한변의 길이
+    start_x, start_y = map(int, sys.stdin.readline().split())  # 나이트의 현재 x,y좌표
     end_x, end_y = map(int, sys.stdin.readline().split())  # 나이트의 목적 x,y좌표
     
-    visited = [[0] * l for _ in range(l)] # 방문 여부를 저장할 배열 초기화
+    visited = [[0] * l for _ in range(l)]  # 방문 여부를 저장할 배열 초기화
     visited[start_x][start_y] = 1
     
-    print(bfs(start_x, start_y, end_x, end_y)) # 최소 이동 횟수 출력
+    print(bfs(start_x, start_y, end_x, end_y))  # 최소 이동 횟수 출력
