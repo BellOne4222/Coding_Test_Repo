35598e47b8c6b8259b2f066f246443b038093645
JongWon Kim <127610150+BellOne4222@users.noreply.github.com>
2024-02-21 19:03:24 +0900

    2/21

--- practice copy.py
+++ practice copy.py
@@ -1,53 +1,47 @@
 import sys
 from collections import deque
 
-def bfs(x,y,trashes):
-    trash = 0
-    
+def bfs(x, y, arr, visited):
     dx = [1,-1,0,0]
     dy = [0,0,1,-1]
     
+    color = arr[x][y]
     queue = deque()
-    queue.append([x,y])
+    queue.append((x, y))
     visited[x][y] = True
     
     while queue:
         cur_x, cur_y = queue.popleft()
-        trash += 1
         
         for i in range(4):
             nx = cur_x + dx[i]
             ny = cur_y + dy[i]
             
-            if 0 <= nx < n and 0 <= ny < m:
-                if not visited[nx][ny]:
-                    if graph[nx][ny] == 1:
-                        visited[nx][ny] = True
-                        queue.append([nx,ny])
-    
-    trashes.append(trash)
-    
-    return trashes
-    
-    
-n,m,k = map(int, sys.stdin.readline().split())
+            if 0 <= nx < N and 0 <= ny < N:
+                if arr[nx][ny] == color and not visited[nx][ny]:
+                    visited[nx][ny] = True
+                    queue.append((nx, ny))
 
-graph = [[0 for _ in range(m)] for _ in range(n)] # [[1, 0, 0, 0], [0, 1, 1, 0], [1, 1, 0, 0]]
+N = int(sys.stdin.readline())
+normal = [list(sys.stdin.readline().strip()) for _ in range(N)]
+color_weakness = [[y if y != 'G' else 'R' for y in x] for x in normal]
 
-for _ in range(k):
-    dump_x, dump_y = map(int, sys.stdin.readline().split())
-    graph[dump_x-1][dump_y-1] = 1
+normal_visited = [[False]*N for _ in range(N)]
+color_weakness_visited = [[False]*N for _ in range(N)]
 
-visited = [[False for _ in range(m)] for _ in range(n)]
+normal_cnt = color_weakness_cnt = 0
 
-trashes = []
+for i in range(N):
+    for j in range(N):
+        if not normal_visited[i][j]:
+            bfs(i, j, normal, normal_visited)
+            normal_cnt += 1
+        if not color_weakness_visited[i][j]:
+            bfs(i, j, color_weakness, color_weakness_visited)
+            color_weakness_cnt += 1
 
-for i in range(n):
-    for j in range(m):
-        if graph[i][j] == 1 and not visited[i][j]:
-            bfs(i,j,trashes)
+print(normal_cnt, color_weakness_cnt)
 
-print(max(trashes))
 
 
 
