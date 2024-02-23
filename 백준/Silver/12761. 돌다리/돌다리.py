3cfe72c1a717a486bce5011a96a0626686da5904
JongWon Kim <127610150+BellOne4222@users.noreply.github.com>
2024-02-23 17:39:05 +0900

    [Silver I] Title: 돌다리, Time: 160 ms, Memory: 34068 KB -BaekjoonHub

---
+++ 백준/Silver/12761. 돌다리/돌다리.py
@@ -0,0 +1,36 @@
+import sys
+from collections import deque
+
+def bfs(x):
+    dy = [1,-1,a,-a,b,-b,a,b]
+    queue = deque()
+    queue.append(x)
+    visited[x] = True
+    
+    while queue:
+        cur_x = queue.popleft()
+        
+        for i in range(8):
+            if i < 6:
+                nx = cur_x + dy[i]
+            else:
+                nx = cur_x * dy[i]
+            
+            if 0 <= nx <= 100000:
+                if not visited[nx]:
+                    visited[nx] = True
+                    queue.append(nx)
+                    bridge[nx] = bridge[cur_x] + 1
+
+
+    
+    
+
+a,b,n,m = map(int, sys.stdin.readline().split())
+
+bridge = [0 for _ in range(100001)]
+visited = [False for _ in range(100001)]
+
+bfs(n)
+
+print(bridge[m])
