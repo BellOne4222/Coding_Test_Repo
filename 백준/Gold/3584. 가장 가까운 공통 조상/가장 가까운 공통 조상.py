adad56a0e7ef86b4bdf63c9943f961ee2c80272e
JongWon Kim <127610150+BellOne4222@users.noreply.github.com>
2024-02-28 16:45:59 +0900

    [Gold IV] Title: 가장 가까운 공통 조상, Time: 88 ms, Memory: 33308 KB -BaekjoonHub

--- 백준/Gold/3584. 가장 가까운 공통 조상/가장 가까운 공통 조상.py
+++ 백준/Gold/3584. 가장 가까운 공통 조상/가장 가까운 공통 조상.py
@@ -1,51 +1,42 @@
 import sys
 sys.setrecursionlimit(10**6)
 
-T = int(input())
+def dfs(vertex,visited):
+    if graph[vertex]:
+        visited.append(graph[vertex][0])
+        dfs(graph[vertex][0],visited)
 
-for t in range(T):
+t = int(sys.stdin.readline())
 
+for _ in range(t):
+    n = int(sys.stdin.readline())
+    graph = [[] for _ in range(n + 1)]
+    visited_start = []
+    visited_end = []
+    for i in range(n-1):
+        start, end = map(int, sys.stdin.readline().split())
+        graph[end].append(start)
+    
+    start_v, end_v = map(int, sys.stdin.readline().split())
+    visited_start.append(start_v)
+    visited_end.append(end_v)
+    dfs(start_v,visited_start)
+    dfs(end_v,visited_end)
+            
 
-    N = int(input())
-
-    graph = [ [] for _ in range(N+1) ]
-
-    for i in range(N-1):
-        A,B = map(int,input().split())
-
-        graph[B].append(A)
-
-
-
-    check1,check2 = map(int,input().split())
-
-    def dfs(x,root):
-
-        if graph[x]:
-
-            root.append(graph[x][0])
-
-            dfs(graph[x][0],root)
-
-    root1=[check1]
-    root2=[check2]
-
-    dfs(check1,root1)
-
-    dfs(check2,root2)
+    parent_vertex = 0
 
     while True:
-
-        if not root1 or not root2:
-            print(temt)
+        if not visited_start or not visited_end:
+            print(parent_vertex)
             break
-
-        elem1 = root1.pop()
-        elem2 = root2.pop()
-
-        if elem1==elem2:
-            temt = elem1
-            continue
+        
+        start_vertex = visited_start.pop()
+        end_vertex = visited_end.pop()
+        
+        if start_vertex != end_vertex:
+            print(parent_vertex)
+            break
         else:
-            print(temt)
-            break
+            parent_vertex = start_vertex
+            continue
