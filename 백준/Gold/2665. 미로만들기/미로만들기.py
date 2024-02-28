f86f806cdf03556750f010e7e0aef3c98ea510ae
JongWon Kim <127610150+BellOne4222@users.noreply.github.com>
2024-02-28 17:57:36 +0900

    [Gold IV] Title: 미로만들기, Time: 88 ms, Memory: 34068 KB -BaekjoonHub

--- 백준/Gold/2665. 미로만들기/미로만들기.py
+++ 백준/Gold/2665. 미로만들기/미로만들기.py
@@ -1,25 +1,33 @@
 import sys
 from collections import deque
-dx = [0, 0, 1, -1] # 동서남북
-dy = [1, -1, 0, 0] # 동서남북
-input = sys.stdin.readline
-n = int(input())
-arr = [list(input().rstrip()) for _ in range(n)]
-cnt = [[1e9]*n for _ in range(n)] # 흰 방으로 바꾼 횟수
-queue = deque()
-queue.append((0, 0))
-cnt[0][0]=0
-while queue:
-    x, y = queue.popleft()
-    for i in range(4):
-        nx = x+dx[i]
-        ny = y+dy[i]
-        if 0<=nx<n and 0<=ny<n:
-            if arr[nx][ny]=='1' and cnt[nx][ny] > cnt[x][y]:
-                cnt[nx][ny] = cnt[x][y]
-                queue.append((nx, ny))
-            elif arr[nx][ny]=='0' and cnt[nx][ny] > cnt[x][y]: 
-                cnt[nx][ny] = cnt[x][y]+1
-                queue.append((nx, ny))
 
-print(cnt[n-1][n-1])
+def bfs(x,y):
+    dx = [1,-1,0,0]
+    dy = [0,0,1,-1]
+    
+    queue = deque()
+    queue.append((x,y))
+    visited[x][y] = 0  # 시작 위치의 visited 값을 0으로 설정
+    
+    while queue:
+        cur_x, cur_y = queue.popleft()
+        
+        for i in range(4):
+            nx = cur_x + dx[i]
+            ny = cur_y + dy[i]
+            if 0 <= nx < n and 0 <= ny < n:
+                if graph[nx][ny] == 0 and visited[nx][ny] > visited[cur_x][cur_y]:
+                    visited[nx][ny] = visited[cur_x][cur_y] + 1
+                    queue.append((nx,ny))
+                    
+                elif graph[nx][ny] == 1 and visited[nx][ny] > visited[cur_x][cur_y]:
+                    visited[nx][ny] = visited[cur_x][cur_y]
+                    queue.append((nx,ny))
+
+n = int(sys.stdin.readline())
+graph = [list(map(int, sys.stdin.readline().rstrip())) for _ in range(n)]
+visited = [[1e9]*n for _ in range(n)]  # visited 리스트를 float('inf')로 초기화
+
+bfs(0,0)
+
+print(visited[n-1][n-1])
