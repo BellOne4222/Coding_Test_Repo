648f88a77393f79043558d5dfb1fa99899041724
JongWon Kim <127610150+BellOne4222@users.noreply.github.com>
2024-03-25 16:16:02 +0900

    3/25

--- practice copy 2.py
+++ practice copy 2.py
@@ -1,43 +1,52 @@
 import sys
-import heapq
 
-input = sys.stdin.readline
+def turn_switch(idx):
+    if switches[idx] == 0:
+        switches[idx] = 1
+    elif switches[idx] == 1:
+        switches[idx] = 0
 
-n = int(input())
-dx = [0, 1, -1, 0]
-dy = [1, 0, 0, -1]
-# 정답 출력을 위한 cnt 변수
-cnt = 0
-# n이 0이면 종료
-while n != 0:
-    cnt += 1
-    # 입력
-    board = [list(map(int, input().split())) for _ in range(n)]
-    # 최소 비용으로 정렬해 줄 heap queue (이것을 기준으로 탐색)
-    heap = []
-    # 가중치를 저장해줄 distance 테이블
-    dist = [[1e9] * n for _ in range(n)]
-    # [0][0]부터 시작
-    dist[0][0] = board[0][0]
-    heapq.heappush(heap, (board[0][0], 0, 0))
 
-    while heap:
-        distance, y, x = heapq.heappop(heap)
+switch_num = int(sys.stdin.readline())
 
-		#최소 가중치를 먼저 가기 때문에 [N-1][N-1]에 도착하면 바로 리턴
-        if y == n-1 and x == n-1:
-            print("Problem", str(cnt)+":", distance)
-            # n 변수 다시 받고 break
-            n = int(input())
-            break
-		# 상하좌우 방향 탐색
-        for i in range(4):
-            ny = y + dy[i]
-            nx = x + dx[i]
-			
-            if 0 <= ny < n and 0 <= nx < n:
-                cost = distance + board[ny][nx]
-                # 이미 저장된 가중치보다 현재 가중치가 낮으면 업데이트
-                if dist[ny][nx] > cost:
-                    dist[ny][nx] = distance + board[ny][nx]
-                    heapq.heappush(heap, (distance + board[ny][nx], ny, nx))
+origin_switches = [-1]
+
+switches = origin_switches + list(map(int, sys.stdin.readline().split()))
+
+student_num = int(sys.stdin.readline())
+
+for _ in range(student_num):
+    male, given_num = map(int, sys.stdin.readline().split())
+    
+    if male == 1:
+        for i in range(0, switch_num, given_num):
+            if i != 0 and i % given_num == 0:
+                turn_switch(i)
+    
+    elif male == 2:
+        cur_switch = switches[given_num]
+        left_idx = given_num - 1
+        right_idx = given_num + 1
+        
+        while True:
+            if left_idx > 0 and right_idx < (switch_num + 1):
+                if switches[left_idx] == switches[right_idx]:
+                    turn_switch(left_idx)
+                    turn_switch(right_idx)
+                    left_idx -= 1
+                    right_idx += 1
+                
+                else:
+                    turn_switch(given_num)
+                    break
+            else:
+                turn_switch(given_num)
+                break
+
+result_idx = 1
+while result_idx <= switch_num:
+    print(switches[result_idx], end=" ")
+    result_idx += 1
+    
+    if result_idx % 20 == 0:
+        print()
--- practice copy.py
+++ practice copy.py
@@ -1,23 +1,55 @@
 import sys
 
-t = int(sys.stdin.readline())
+def turn_switch(idx):
+  if switches[idx] == 0:
+    switches[idx] = 1
+  elif switches[idx] == 1:
+    switches[idx] = 0
 
-for _ in range(t):
-  n = int(sys.stdin.readline())
+
+switch_num = int(sys.stdin.readline())
+
+origin_switches = [-1]
+
+switches = origin_switches + list(map(int, sys.stdin.readline().split()))
+
+student_num = int(sys.stdin.readline())
+
+for _ in range(student_num):
+  male, given_num = map(int, sys.stdin.readline().split())
   
-  cases = [] # ['113', '12340', '12345', '98346', '123440']
-  for i in range(n):
-    cases.append(sys.stdin.readline().rstrip())
+  if male == 1:
+    for i in range(0, switch_num, given_num):
+      if i != 0 and i % given_num == 0:
+        turn_switch(i)
   
-  cases.sort()
-  
-  flag = "YES"
-  
-  for j in range(n-1):
-    if cases[j] == cases[j+1][:len(cases[j])]:
-      flag = "NO"
-      break
-  
-  print(flag)
+  elif male == 2:
+    cur_switch = switches[given_num]
+    left_idx = given_num - 1
+    right_idx = given_num + 1
     
+    while True:
+      if left_idx > 0 and right_idx < (switch_num + 1):
+        if switches[left_idx] == switches[right_idx]:
+          turn_switch(left_idx)
+          turn_switch(right_idx)
+          left_idx -= 1
+          right_idx += 1
+        
+        else:
+          turn_switch(given_num)
+          break
+      else:
+        turn_switch(given_num)
+        break
+
+result_idx = 1
+while result_idx <= switch_num:
+  print(switches[result_idx], end=" ")
+  result_idx += 1
+  
+  if result_idx % 20 == 0:
+    print()
+  
+
     
