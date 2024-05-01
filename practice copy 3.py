e1826dd052a3fb8214abc5d6d03ee7540c004da3
JongWon Kim <127610150+BellOne4222@users.noreply.github.com>
2024-05-01 16:35:59 +0900

    5/1

--- practice copy 3.py
+++ practice copy 3.py
@@ -1,60 +1,27 @@
-def case_one(amountText, flag):
-    for t in amountText:
-        if not (t.isdigit() or t == ","):
-            flag = False
-            break
-    return flag
-
-def case_two(amountText, flag):
-    for i in range(len(amountText)):
-        if amountText[0] == "0" or amountText[0] == "," or amountText[-1] == ",":
-            flag = False
-            break
-    return flag
-
-def case_three(amountText, flag):
-    cnt = 0
-    for j in reversed(range(len(amountText))):
-        if amountText[j] == ",":
-            if cnt != 3:
-                flag = False
-                break
-            else:
-                cnt = 0
-        else:
-            cnt += 1
-    return flag
-
-
-def solution(amountText):
+def solution(dots, lines):
+    # 점과 선분을 오름차순으로 정렬
+    dots.sort()
+    lines.sort(reverse=True)
     
-    # 1. 숫자 or ,
-
-    # 2. 0원아니면 가장 왼쪽에 0 x
-
-    # 3. 구분자 ,가 없어도 옳은 금액
-
-    # 4. 구분자는 세자리 숫자에만 사용
-
-    # 5. 왼쪽 끝 or 오른쪽 끝에는 구분자 x
-
-    flag = True
-
-    case_one(amountText, flag)
-
-    if not flag:
-        return False
-    else:
-        case_two(amountText, flag)       
-
-    if not flag:
-        return False
-    else:
-        case_three(amountText, flag) 
+    covered = 0  # 덮인 점의 수
+    segments_used = 0  # 사용된 선분의 수
+    i = 0  # 점 배열의 인덱스
     
-    if not flag:
-        return False
-    else:
-        return True
+    # 모든 점이 덮힐 때까지 반복
+    while covered < len(dots):
+        segments_used += 1  # 선분 하나를 더 사용
+        start = dots[i]  # 현재 덮을 점의 위치
+        
+        # 가장 긴 선분으로 가능한 많은 점을 덮기
+        for line in lines:
+            # 현재 위치에서 시작하여 선분으로 덮을 수 있는 가장 먼 점 찾기
+            while i < len(dots) and dots[i] <= start + line:
+                i += 1
+            break
+        
+        # 덮인 점의 수 업데이트
+        covered = i
+        
+    return segments_used
 
-print(solution("39,00"))
+print(solution([1,3,4,6,7,10], [2,2,2,2]))
--- practice copy.py
+++ practice copy.py
@@ -1,15 +1,30 @@
-amountText = "39,00"
-flag = True
+# grade = [3,2,1,2]
+grade = [2,2,1]
 
-cnt = 0
-for j in reversed(range(len(amountText))):
-    if amountText[j] == ",":
-        if cnt != 3:
-            flag = False
-            break
-        else:
-            cnt = 0
+sorted_grade = sorted(grade, reverse=True)
+
+grade_dict = {}
+
+cur_grade = 1
+
+student = 0
+
+highest_grade = sorted_grade[0]
+
+result = []
+
+for i in range(len(grade)):
+    if sorted_grade[i] == highest_grade:
+        student += 1
     else:
-        cnt += 1
+        cur_grade += student
+        student = 1
+        highest_grade = sorted_grade[i]
 
-print(flag)
+    if sorted_grade[i] not in grade_dict:
+        grade_dict[sorted_grade[i]] = cur_grade
+
+for j in range(len(grade)):
+    result.append(grade_dict[grade[j]])
+
+print(result)
