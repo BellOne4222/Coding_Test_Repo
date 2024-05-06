9e8a35485f61aaff21f333094a1fcb958025ba0a
JongWon Kim <127610150+BellOne4222@users.noreply.github.com>
2024-05-06 15:11:09 +0900

    [level 0] Title: 배열 뒤집기, Time: 0.01 ms, Memory: 76.3 MB -BaekjoonHub

---
+++ 프로그래머스/0/120821. 배열 뒤집기/배열 뒤집기.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int[] solution(int[] num_list) {
+        int[] answer = new int[num_list.length];
+        int idx = 0;
+        
+        
+        for (int i = num_list.length - 1; i >= 0; i--){
+            answer[idx] = num_list[i];
+            idx++;
+        }
+        
+        return answer;
+    }
+}
