37db24b398a0ca2da435206480cfdb1b95483469
JongWon Kim <127610150+BellOne4222@users.noreply.github.com>
2024-08-05 18:18:36 +0900

    8/5

--- practice copy.py
+++ practice copy.py
@@ -1,40 +1,23 @@
-import sys
+H, W = map(int, input().split())
+N = int(input())
+stickers = [tuple(map(int, input().split())) for _ in range(N)]
 
-n = int(sys.stdin.readline())
+max_area = 0
 
-n_nums = list(map(int, sys.stdin.readline().rstrip().split()))
+for i in range(N):
+    for j in range(i + 1, N):
+        R1, C1 = stickers[i]
+        R2, C2 = stickers[j]
 
-m = int(sys.stdin.readline())
+        for (r1, c1) in [(R1, C1), (C1, R1)]:
+            for (r2, c2) in [(R2, C2), (C2, R2)]:
+                if (r1 + r2 <= H and max(c1, c2) <= W) or (c1 + c2 <= W and max(r1, r2) <= H):
+                    max_area = max(max_area, r1 * c1 + r2 * c2)
+                if (r1 + c2 <= H and max(c1, r2) <= W) or (c1 + r2 <= W and max(r1, c2) <= H):
+                    max_area = max(max_area, r1 * c1 + r2 * c2)
+                if (c1 + r2 <= H and max(r1, c2) <= W) or (r1 + c2 <= W and max(c1, r2) <= H):
+                    max_area = max(max_area, r1 * c1 + r2 * c2)
+                if (c1 + c2 <= H and max(r1, r2) <= W) or (r1 + r2 <= W and max(c1, c2) <= H):
+                    max_area = max(max_area, r1 * c1 + r2 * c2)
 
-num_cards = list(map(int, sys.stdin.readline().rstrip().split()))
-
-n_nums.sort()
-
-answer = []
-
-for card in num_cards:
-    target = card
-    left = 0
-    right = n-1
-    flag = False
-    
-    while left <= right:
-        mid_idx = (left + right) // 2
-        
-        if n_nums[mid_idx] == target:
-            answer.append(1)
-            flag = True
-            break
-        
-        elif n_nums[mid_idx] > target:
-            right = mid_idx - 1
-        
-        else:
-            left = mid_idx + 1
-    
-    if not flag:
-        answer.append(0)
-    
-
-print(" ".join(map(str, answer)))        
-        
+print(max_area)
