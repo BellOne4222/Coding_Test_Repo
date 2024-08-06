8335d40e594788ba4078b3e8387de76cd6c67fc9
JongWon Kim <127610150+BellOne4222@users.noreply.github.com>
2024-08-06 18:23:27 +0900

    [Silver II] Title: 부분수열의 합, Time: 528 ms, Memory: 80888 KB -BaekjoonHub

---
+++ 백준/Silver/1182. 부분수열의 합/부분수열의 합.py
@@ -0,0 +1,20 @@
+import sys
+from itertools import combinations
+
+N, S = map(int, sys.stdin.readline().split())
+
+nums = list(map(int, sys.stdin.readline().split()))
+
+cnt = 0
+
+for i in range(1, N+1):
+    comb_nums = list(combinations(nums, i))
+    
+    for j in comb_nums:
+        if len(j) < 0:
+            continue
+        else:
+            if sum(j) == S:
+                cnt += 1
+
+print(cnt)
