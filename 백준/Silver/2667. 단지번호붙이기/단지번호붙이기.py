f165aab24d5fa1875b26a9ba0639f524e410430a
JongWon Kim <127610150+BellOne4222@users.noreply.github.com>
2024-08-12 13:24:17 +0900

    [Silver I] Title: 단지번호붙이기, Time: 44 ms, Memory: 31332 KB -BaekjoonHub

--- 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.py
+++ 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.py
@@ -1,47 +1,47 @@
-import sys
-from collections import deque
+# dfs
 
-# BFS 함수 정의
-def bfs(x, y, graph):
-    house = 1  # 단지 내 집의 수를 저장할 변수
-    queue = deque()  # BFS를 위한 큐 생성
-    queue.append([x, y])  # 시작점 추가
-    visited[x][y] = True  # 시작점 방문 표시
+n = int(input())
+graph = []
+num = []
 
-    dx = [1, -1, 0, 0]  # 이동할 수 있는 방향 (상, 하, 좌, 우)
-    dy = [0, 0, 1, -1]
+for i in range(n):
+    graph.append(list(map(int, input())))
 
-    while queue:
-        cur_x, cur_y = queue.popleft()  # 큐에서 현재 위치를 꺼내옴
-
-        for i in range(4):  # 상하좌우 방향에 대해 탐색
-            nx = cur_x + dx[i]
-            ny = cur_y + dy[i]
-            if 0 <= nx < n and 0 <= ny < n and graph[nx][ny] == 1:
-                if not visited[nx][ny]:  # 방문하지 않은 집인 경우
-                    queue.append([nx, ny])  # 다음 위치를 큐에 추가
-                    visited[nx][ny] = True  # 해당 위치를 방문했다고 표시
-                    house += 1  # 집의 수 증가
-                    
-    return house  # 현재 단지의 집 수 반환
+dx = [0, 0, 1, -1]
+dy = [1, -1, 0, 0]
 
 
-n = int(sys.stdin.readline())  # 지도의 크기 입력
+def DFS(x, y):
+    if x < 0 or x >= n or y < 0 or y >= n:
+        return False
 
-graph = [list(map(int, sys.stdin.readline().rstrip())) for _ in range(n)]  # 지도 정보 입력
+    if graph[x][y] == 1:
+        global count
+        count += 1
+        graph[x][y] = 0
+        for i in range(4):
+            nx = x + dx[i]
+            ny = y + dy[i]
+            DFS(nx, ny)
+        return True
+    return False
 
-visited = [[False for _ in range(n)] for _ in range(n)]  # 방문 여부를 저장할 2차원 리스트 초기화
 
-houses = []  # 각 단지 내 집의 수를 저장할 리스트
+count = 0
+result = 0
 
-# 모든 지점에 대해 탐색
 for i in range(n):
     for j in range(n):
-        if graph[i][j] == 1 and not visited[i][j]:  # 집이 있고 아직 방문하지 않은 곳인 경우
-            houses.append(bfs(i, j, graph))  # BFS 탐색을 시작하고 집의 수를 리스트에 추가
+        if DFS(i, j) == True:
+            num.append(count)
+            result += 1
+            count = 0
 
-houses.sort()  # 집의 수를 오름차순으로 정렬
+num.sort()
+print(result)
+for i in range(len(num)):
+    print(num[i])
 
-print(len(houses))  # 총 단지 수 출력
-for k in range(len(houses)):
-    print(houses[k])  # 각 단지 내 집의 수 출력
+
+            
+
