828925eeb5dc0be7ae7db6d190e169533764485b
JongWon Kim <127610150+BellOne4222@users.noreply.github.com>
2024-08-14 15:34:03 +0900

    [Silver II] Title: 예산, Time: 52 ms, Memory: 32140 KB -BaekjoonHub

---
+++ 백준/Silver/2512. 예산/예산.py
@@ -0,0 +1,31 @@
+import sys
+
+n = int(sys.stdin.readline())
+
+nums = list(map(int, sys.stdin.readline().split()))
+nums.sort()
+
+m = int(sys.stdin.readline())
+
+start = 0
+end = max(nums)
+
+
+while start <= end:
+    result = 0
+    
+    mid = (start + end) // 2
+    
+    for num in nums:
+        if mid >= num:
+            result += num
+        else:
+            result += mid
+    
+    if result > m:
+        end = mid - 1
+    else:
+        start = mid + 1
+
+print(end)
+        
