0dfc8c52ce8887a67d34af0d674b0572b9e54d78
JongWon Kim <127610150+BellOne4222@users.noreply.github.com>
2024-08-17 22:25:28 +0900

    [Silver III] Title: 계단 오르기, Time: 32 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/2579. 계단 오르기/계단 오르기.py
+++ 백준/Silver/2579. 계단 오르기/계단 오르기.py
@@ -1,14 +1,22 @@
-N = int(input())
-stairs = [0]
-dp_table = [0] * (N+1)
+import sys
 
-for j in range(1,N+1):
-    stairs.append(int(input()))
-    if j == 1 :
-        dp_table[1] = stairs[1]
-    elif j == 2 :
-        dp_table[2] = stairs[1] + stairs[2]
-    else:
-        dp_table[j] = max(stairs[j] + stairs[j-1] + dp_table[j-3], stairs[j] + dp_table[j-2])
+input = sys.stdin.readline
 
-print(dp_table[N])
+n = int(input())
+
+# 계단의 숫자를 초기화 합니다. 1층은 1번째(not 0번째) 인덱스에 저장합니다.
+stairs = [0] * 301
+for i in range(1, n + 1):
+    stairs[i] = int(input())
+
+# dp 배열을 초기화합니다.
+dp = [0] * 301
+dp[1] = stairs[1]
+dp[2] = stairs[1] + stairs[2]
+dp[3] = max(stairs[1] + stairs[3], stairs[2] + stairs[3])
+
+# 점화식을 계산합니다.
+for i in range(4, n + 1):
+    dp[i] = max(dp[i - 3] + stairs[i - 1] + stairs[i], dp[i - 2] + stairs[i])
+
+print(dp[n])
