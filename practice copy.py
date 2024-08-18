d49052ff653912d28d46c979a84472bac72a4180
JongWon Kim <127610150+BellOne4222@users.noreply.github.com>
2024-08-18 15:30:22 +0900

    8/18

--- practice copy.py
+++ practice copy.py
@@ -1,20 +1,14 @@
 import sys
 
-t = int(sys.stdin.readline())
+n = int(sys.stdin.readline())
 
-stairs = [0] * (t + 1)
+a = list(map(int, sys.stdin.readline().split()))
 
-for i in range(1, t + 1):
-    stair = int(sys.stdin.readline())
-    stairs[i] = stair
+dp_table = [1] * 1001
 
-dp_table = [0 for _ in range(t + 1)]
+for i in range(n):
+    for j in range(i):
+        if a[i] < a[j]:
+            dp_table[i] = max(dp_table[i], dp_table[j] + 1)
 
-dp_table[1] = stairs[1]
-dp_table[2] = stairs[1] + stairs[2]
-dp_table[3] = max(stairs[1] + stairs[3], stairs[2] + stairs[3])
-
-for i in range(4, t + 1):
-    dp_table[i] = max(dp_table[i - 3] + stairs[i - 1] + stairs[i], dp_table[i - 2] + stairs[i])
-
-print(dp_table[t])
+print(max(dp_table))
