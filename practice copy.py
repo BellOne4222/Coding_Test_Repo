3bcd53c8e1408671e1c9cbbdab4503c3f245091a
JongWon Kim <127610150+BellOne4222@users.noreply.github.com>
2024-08-25 12:01:03 +0900

    8/25

--- practice copy.py
+++ practice copy.py
@@ -1,53 +1,14 @@
 import sys
-from itertools import combinations
-from collections import deque
 
-n,a,b = map(int, sys.stdin.readline().split())
+n, k = map(int, sys.stdin.readline().split())
+students = list(map(int, sys.stdin.readline().split()))
 
-a_lst = list(map(int, sys.stdin.readline().split()))
+# 각 인접한 원생들의 키 차이를 계산
+differences = []
+for i in range(1, n):
+    differences.append(students[i] - students[i - 1])
 
-b_lst = list(map(int, sys.stdin.readline().split()))
+# 차이들을 내림차순으로 정렬
+differences.sort(reverse=True)
 
-result = []
-
-a_lst.sort(reverse=True)
-b_lst.sort(reverse=True)
-
-a_lst = deque(a_lst)
-b_lst = deque(b_lst)
-
-cnt = n // 2
-no_cnt = n % 2
-
-if n == 1:
-    result.append(a_lst[0])
-else:
-    a_comb = list(combinations(a_lst,2))
-    a_comb.sort(key = lambda x:sum(x), reverse=True)
-    a_comb = deque(a_comb)
-    for i in range(cnt):
-        if sum(a_comb[0]) > b_lst[0]:
-            result.append(sum(a_comb[0]))
-            used_a = a_comb[0][0]
-            used_b = a_comb[0][1]
-            a_lst.rotate(2)
-            for k in range(len(a_comb)):
-                if used_a in a_comb[0] or used_b in a_comb[0]:
-                    a_comb.rotate(1)
-                else:
-                    break
-                
-        else:
-            result.append(b_lst[0])
-            b_lst.rotate(1)
-
-    if no_cnt != 0:
-        for j in range(no_cnt):
-            result.append(a_lst[j])
-    
-print(sum(result))
-    
-        
-        
-    
-    
+print(differences)
--- 백준/Gold/13164. 행복 유치원/행복 유치원.py
+++ 백준/Gold/13164. 행복 유치원/행복 유치원.py
@@ -1,18 +1,25 @@
 import sys
 
+# 입력값 처리: 원생의 수 N과 조의 수 K
 n, k = map(int, sys.stdin.readline().split())
+
+# 원생들의 키를 입력받아 리스트로 저장
 students = list(map(int, sys.stdin.readline().split()))
 
-# 각 인접한 원생들의 키 차이를 계산
+# 인접한 원생들 사이의 키 차이를 저장할 리스트
 differences = []
 for i in range(1, n):
+    # 각 인접한 원생들 사이의 키 차이를 계산하여 리스트에 추가
     differences.append(students[i] - students[i - 1])
 
 # 차이들을 내림차순으로 정렬
+# 큰 차이들을 제거하기 위해 정렬
 differences.sort(reverse=True)
 
-# 가장 큰 차이 K-1개를 선택하여 조로 나눔
-# 이 차이들은 무시하므로 나머지 차이들의 합이 최소가 됨
+# 가장 큰 차이 K-1개를 제거
+# 제거된 차이들은 조 사이의 경계가 되고
+# 나머지 차이들의 합이 최소 비용이 된다.
 result = sum(differences[k-1:])
 
+# 최종 결과 출력
 print(result)
